a305dc979cd92c5b7c4da68bd1e7bdb089bcd905
Alessandro Pregnolato <56503173+AlePre2@users.noreply.github.com>
2023-05-04 01:06:27 +0200

    Fix CurrentNumberOfPlayers and MaxNumberOfPlayers
    Previously the function could return a negative number of "CurrentNumberOfPlayers", as private connections were not considered

--- Source/EOSIntegrationKit/Subsystem/EIK_Subsystem.cpp
+++ Source/EOSIntegrationKit/Subsystem/EIK_Subsystem.cpp
@@ -1010,8 +1010,8 @@ void UEIK_Subsystem::OnFindSessionCompleted(bool bWasSuccess) const
 					}
 					FSessionFindStruct LocalStruct;
 					LocalStruct.SessionName = LocalArraySettings.FindRef("SEARCHKEYWORDS");
-					LocalStruct.CurrentNumberOfPlayers = SessionResult.OnlineResult.Session.SessionSettings.NumPublicConnections - SessionResult.OnlineResult.Session.NumOpenPublicConnections;
-					LocalStruct.MaxNumberOfPlayers = SessionResult.OnlineResult.Session.SessionSettings.NumPublicConnections;
+					LocalStruct.CurrentNumberOfPlayers = (SessionResult.OnlineResult.Session.SessionSettings.NumPublicConnections + SessionResult.OnlineResult.Session.SessionSettings.NumPrivateConnections) - (SessionResult.OnlineResult.Session.NumOpenPublicConnections + SessionResult.OnlineResult.Session.NumOpenPrivateConnections);
+					LocalStruct.MaxNumberOfPlayers = SessionResult.OnlineResult.Session.SessionSettings.NumPublicConnections + SessionResult.OnlineResult.Session.SessionSettings.NumPrivateConnections;
 					LocalStruct.SessionResult= SessionResult;
 					LocalStruct.SessionSettings = LocalArraySettings;
 					LocalStruct.bIsDedicatedServer = false;
