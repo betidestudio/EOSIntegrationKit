cd40ffa08c34a4cfb61ff1708112a6562e2d3aea
Betide Studio <codingwithdevesh@gmail.com>
2023-12-28 13:30:36 +0530

    Adding presence modifactions by Chippy4
    The commit was pushed by @Chippy4
    
    Co-Authored-By: Chippy4 <127122031+chippy4@users.noreply.github.com>

---
+++ Source/OnlineSubsystemEIK/AsyncFunctions/Presence/EIK_SetPresence_AsyncFunction.cpp
@@ -0,0 +1,74 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EIK_SetPresence_AsyncFunction.h"
+#include "IOnlineSubsystemEOS.h"
+#include "Interfaces/OnlineIdentityInterface.h"
+#include "Interfaces/OnlinePresenceInterface.h"
+#include "Online/OnlineSessionNames.h"
+
+void UEIK_SetPresence_AsyncFunction::Activate()
+{
+	SetPresence();
+	Super::Activate(); 
+}
+
+
+void UEIK_SetPresence_AsyncFunction::SetPresence()
+{
+	if (IOnlineSubsystem* SubsystemRef = Online::GetSubsystem(this->GetWorld())) 
+	{
+		//Updating Presence
+		IOnlineSubsystem* Subsystem = Online::GetSubsystem(this->GetWorld());
+		IOnlineIdentityPtr Identity = Subsystem->GetIdentityInterface();
+		IOnlinePresencePtr Presence = Subsystem->GetPresenceInterface();
+		FOnlineUserPresenceStatus Status;
+
+		//Switch On Presence Status
+		switch (PresenceStatus) 
+		{
+		case EPresenceStatus::PR_Online:
+			Status.State = EOnlinePresenceState::Online;
+			UE_LOG(LogTemp, Log, TEXT("Presence Set To Online"));
+			break;
+		case EPresenceStatus::PR_Offline:
+			UE_LOG(LogTemp, Error, TEXT("Cant Change Presence To Offline When User Is Playing"));
+			break;
+		case EPresenceStatus::PR_Away:
+			Status.State = EOnlinePresenceState::Away;
+			UE_LOG(LogTemp, Error, TEXT("Presence Set To Away"));
+			break;
+		case EPresenceStatus::PR_ExtendedAway:
+			Status.State = EOnlinePresenceState::ExtendedAway;
+			UE_LOG(LogTemp, Error, TEXT("Presence Set To ExtendedAway"));
+			break;
+		case EPresenceStatus::PR_DoNotDisturb:
+			Status.State = EOnlinePresenceState::DoNotDisturb;
+			UE_LOG(LogTemp, Error, TEXT("Presence Set To DoNotDisturb"));
+			break;
+		}
+		
+		FString RichPresenceStatus = RichPresense;
+		Status.StatusStr = RichPresenceStatus;
+		UE_LOG(LogTemp, Warning, TEXT("Presence Updated With Status: %s"), *RichPresenceStatus);
+		Presence->SetPresence(
+			*Identity->GetUniquePlayerId(0).Get(),
+			Status,
+			IOnlinePresence::FOnPresenceTaskCompleteDelegate::CreateLambda([](
+				const class FUniqueNetId& UserId,
+				const bool bWasSuccessful)
+				{
+					if (bWasSuccessful) // Check bWasSuccessful.
+						UE_LOG(LogTemp, Log, TEXT("Presence Updated Successfully"));
+				}));
+		SetReadyToDestroy();
+	}
+}
+
+UEIK_SetPresence_AsyncFunction* UEIK_SetPresence_AsyncFunction::SetEOSPresence(FString RichPresense, EPresenceStatus PresenceStatus)
+{
+	UEIK_SetPresence_AsyncFunction* Ueik_SetPresenceObject = NewObject< UEIK_SetPresence_AsyncFunction>();
+	Ueik_SetPresenceObject->RichPresense = RichPresense;
+	Ueik_SetPresenceObject->PresenceStatus = PresenceStatus;
+	return Ueik_SetPresenceObject;
+}
---
+++ Source/OnlineSubsystemEIK/AsyncFunctions/Presence/EIK_SetPresence_AsyncFunction.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "OnlineSubsystemEIK/Subsystem/EIK_Subsystem.h"
+#include "IOnlineSubsystemEOS.h"
+#include "OnlineSubsystemUtils.h"
+#include "OnlineSubsystem.h"
+#include "Interfaces/OnlineIdentityInterface.h"
+#include "Interfaces/OnlinePresenceInterface.h"
+#include "EIK_SetPresence_AsyncFunction.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ONLINESUBSYSTEMEIK_API UEIK_SetPresence_AsyncFunction : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+public:
+	FString RichPresense;
+	EPresenceStatus PresenceStatus;
+
+	virtual void Activate() override;
+
+	void SetPresence();
+
+	UFUNCTION(BlueprintCallable, DisplayName = "Set EOS Presence", meta = (BlueprintInternalUseOnly = "true"), Category = "EOS Integration Kit || Presence")
+		static UEIK_SetPresence_AsyncFunction* SetEOSPresence(FString RichPresense, EPresenceStatus PresenceStatus);
+
+};
