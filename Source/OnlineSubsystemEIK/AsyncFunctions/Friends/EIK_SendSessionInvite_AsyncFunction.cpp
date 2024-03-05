91550a0ed735646b30e9158225b790bbdd335004
Betide Studio <codingwithdevesh@gmail.com>
2024-03-05 12:04:09 +0530

    Send Session Invite for players

---
+++ Source/OnlineSubsystemEIK/AsyncFunctions/Friends/EIK_SendSessionInvite_AsyncFunction.cpp
@@ -0,0 +1,50 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+
+#include "EIK_SendSessionInvite_AsyncFunction.h"
+#include "OnlineSubsystemUtils.h"
+
+UEIK_SendSessionInvite_AsyncFunction* UEIK_SendSessionInvite_AsyncFunction::SendSessionInvite(FEIKUniqueNetId FriendId)
+{
+	UEIK_SendSessionInvite_AsyncFunction* BlueprintNode = NewObject<UEIK_SendSessionInvite_AsyncFunction>();
+	BlueprintNode->Var_FriendId = FriendId;
+	return BlueprintNode;
+}
+
+void UEIK_SendSessionInvite_AsyncFunction::Activate()
+{
+	Super::Activate();
+	if(const IOnlineSubsystem *SubsystemRef = Online::GetSubsystem(GetWorld()))
+	{
+		if(const IOnlineSessionPtr SessionInterface = SubsystemRef->GetSessionInterface())
+		{
+			if(SessionInterface->SendSessionInviteToFriend(0,NAME_GameSession,*Var_FriendId.GetUniqueNetId()))
+			{
+				SetReadyToDestroy();
+				MarkAsGarbage();
+				OnSuccess.Broadcast("");
+			}
+			else
+			{
+				// Failed to send session invite
+				SetReadyToDestroy();
+				MarkAsGarbage();
+				OnFailure.Broadcast("Failed to send session invite");
+			}
+		}
+		else
+		{
+			// Failed to get Session Interface
+			SetReadyToDestroy();
+			MarkAsGarbage();
+			OnFailure.Broadcast("Failed to get Session Interface");
+		}
+	}
+	else
+	{
+		// Failed to get Subsystem
+		SetReadyToDestroy();
+		MarkAsGarbage();
+		OnFailure.Broadcast("Failed to get Subsystem");
+	}
+}
---
+++ Source/OnlineSubsystemEIK/AsyncFunctions/Friends/EIK_SendSessionInvite_AsyncFunction.h
@@ -0,0 +1,30 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "OnlineSubsystemEIK/Subsystem/EIK_Subsystem.h"
+#include "EIK_SendSessionInvite_AsyncFunction.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSendSessionInviteComplete, const FString&, Error);
+UCLASS()
+class ONLINESUBSYSTEMEIK_API UEIK_SendSessionInvite_AsyncFunction : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"), Category="EOS Integration Kit || Friends", DisplayName = "Send Session Invite")
+	static UEIK_SendSessionInvite_AsyncFunction* SendSessionInvite(FEIKUniqueNetId FriendId);
+
+private:
+	FEIKUniqueNetId Var_FriendId;
+	FString Var_SessionName;
+	virtual void Activate() override;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnSendSessionInviteComplete OnSuccess;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnSendSessionInviteComplete OnFailure;	
+};
