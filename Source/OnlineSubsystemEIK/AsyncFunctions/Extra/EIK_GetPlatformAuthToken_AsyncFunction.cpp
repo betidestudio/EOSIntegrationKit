34b404dad579c995c4fd0515527d19eacd15a3b3
Betide Studio <codingwithdevesh@gmail.com>
2024-05-12 01:39:45 +0530

    Added GetPlatformAuthToken for Steam

---
+++ Source/OnlineSubsystemEIK/AsyncFunctions/Extra/EIK_GetPlatformAuthToken_AsyncFunction.cpp
@@ -0,0 +1,53 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+
+#include "EIK_GetPlatformAuthToken_AsyncFunction.h"
+#include "Interfaces/OnlineIdentityInterface.h"
+#include "OnlineSubsystem.h"
+#include "OnlineSubsystemEOS.h"
+#include "UserManagerEOS.h"
+
+UEIK_GetPlatformAuthToken_AsyncFunction* UEIK_GetPlatformAuthToken_AsyncFunction::GetPlatformAuthToken(
+	TEnumAsByte<EEIK_PlatformToUse> PlatformToUse)
+{
+	UEIK_GetPlatformAuthToken_AsyncFunction* Node = NewObject<UEIK_GetPlatformAuthToken_AsyncFunction>();
+	Node->PlatformToUse = PlatformToUse;
+	return Node;
+}
+
+void UEIK_GetPlatformAuthToken_AsyncFunction::OnGetPlatformAuthTokenComplete(int I, bool bArg,
+                                                                             const FExternalAuthToken& ExternalAuthToken)
+{
+	if(bArg)
+	{
+		OnSuccess.Broadcast(ExternalAuthToken.TokenString);
+	}
+	else
+	{
+		OnFailure.Broadcast(ExternalAuthToken.TokenString);
+	}
+	SetReadyToDestroy();
+	MarkAsGarbage();
+}
+
+void UEIK_GetPlatformAuthToken_AsyncFunction::Activate()
+{
+	Super::Activate();
+	if(PlatformToUse == EEIK_PlatformToUse::Steam)
+	{
+		if(	IOnlineSubsystem* OnlineSub = IOnlineSubsystem::Get())
+		{
+			if (FOnlineSubsystemEOS* EOSRef = static_cast<FOnlineSubsystemEOS*>(OnlineSub))
+			{
+				if(FUserManagerEOS* UserManager = EOSRef->UserManager.Get())
+				{
+					FUserManagerEOS::FOnGetLinkedAccountAuthTokenCompleteDelegate Delegate = FUserManagerEOS::FOnGetLinkedAccountAuthTokenCompleteDelegate::CreateUObject(this, &UEIK_GetPlatformAuthToken_AsyncFunction::OnGetPlatformAuthTokenComplete);
+					UserManager->GetPlatformAuthToken(0,Delegate);
+					return;
+				}
+			}
+		}
+	}
+	SetReadyToDestroy();
+	MarkAsGarbage();
+}
---
+++ Source/OnlineSubsystemEIK/AsyncFunctions/Extra/EIK_GetPlatformAuthToken_AsyncFunction.h
@@ -0,0 +1,38 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Interfaces/OnlineIdentityInterface.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "EIK_GetPlatformAuthToken_AsyncFunction.generated.h"
+
+UENUM(BlueprintType)
+enum EEIK_PlatformToUse
+{
+	Steam,
+	Apple,
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnGetPlatformAuthTokenComplete, const FString&, AuthToken);
+UCLASS()
+class ONLINESUBSYSTEMEIK_API UEIK_GetPlatformAuthToken_AsyncFunction : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"))
+	static UEIK_GetPlatformAuthToken_AsyncFunction* GetPlatformAuthToken(TEnumAsByte<EEIK_PlatformToUse> PlatformToUse);
+	
+	UPROPERTY(BlueprintAssignable)
+	FOnGetPlatformAuthTokenComplete OnSuccess;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnGetPlatformAuthTokenComplete OnFailure;
+private:
+	TEnumAsByte<EEIK_PlatformToUse> PlatformToUse;
+	void OnGetPlatformAuthTokenComplete(int I, bool bArg, const FExternalAuthToken& ExternalAuthToken);
+	virtual void Activate() override;
+	
+};
