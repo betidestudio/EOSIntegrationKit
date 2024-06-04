250f111aebc283c36c045a6831bc24878ccce3d0
Betide Studio <codingwithdevesh@gmail.com>
2024-06-04 19:55:52 +0530

    EIK Editor Module to manage upcoming Toolbar button and other editor only functionalities

---
+++ Source/EIKEditor/Private/EIKEditor.cpp
@@ -0,0 +1,17 @@
+﻿#include "EIKEditor.h"
+
+#define LOCTEXT_NAMESPACE "FEIKEditorModule"
+
+void FEIKEditorModule::StartupModule()
+{
+    
+}
+
+void FEIKEditorModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FEIKEditorModule, EIKEditor)
