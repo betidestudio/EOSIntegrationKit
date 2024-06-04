c09f50976a2a46a5ebaa4df473511ce954d262cf
Betide Studio <codingwithdevesh@gmail.com>
2024-06-04 19:45:46 +0530

    EOS_Connect_AddNotifyAuthExpiration, EOS_Connect_AddNotifyLoginStatusChanged and EOS_Connect_CopyIdToken added

---
+++ Source/OnlineSubsystemEIK/SdkFunctions/ConnectInterface/EIK_ConnectSubsystem.cpp
@@ -0,0 +1,77 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+
+#include "EIK_ConnectSubsystem.h"
+
+#include "OnlineSubsystem.h"
+#include "OnlineSubsystemEOS.h"
+
+//LogEIK
+DEFINE_LOG_CATEGORY(LogEIK);
+
+void UEIK_ConnectSubsystem::EIK_Connect_AddNotifyAuthExpiration(const FOnAuthExpirationCallback& Callback)
+{
+	if(	IOnlineSubsystem* OnlineSub = IOnlineSubsystem::Get("EIK"))
+	{
+		if (FOnlineSubsystemEOS* EOSRef = static_cast<FOnlineSubsystemEOS*>(OnlineSub))
+		{
+			EOS_Connect_AddNotifyAuthExpirationOptions AddNotifyAuthExpirationOptions = { };
+			AddNotifyAuthExpirationOptions.ApiVersion = EOS_CONNECT_ADDNOTIFYAUTHEXPIRATION_API_LATEST;
+			EOS_Connect_AddNotifyAuthExpiration(EOSRef->ConnectHandle, &AddNotifyAuthExpirationOptions, this, [] (const EOS_Connect_AuthExpirationCallbackInfo* Data)
+			{
+				UEIK_ConnectSubsystem* ConnectSubsystem = static_cast<UEIK_ConnectSubsystem*>(Data->ClientData);
+				if(ConnectSubsystem)
+				{
+					ConnectSubsystem->OnAuthExpiration.ExecuteIfBound(Data->LocalUserId);
+				}
+			});
+			return;
+		}
+	}
+	UE_LOG(LogEIK, Error, TEXT("Failed to add notify auth expiration either OnlineSubsystem is not valid or EOSRef is not valid."));
+}
+
+void UEIK_ConnectSubsystem::EIK_Connect_AddNotifyLoginStatusChanged(const FOnLoginStatusChangedCallback& Callback)
+{
+	if(	IOnlineSubsystem* OnlineSub = IOnlineSubsystem::Get("EIK"))
+	{
+		if (FOnlineSubsystemEOS* EOSRef = static_cast<FOnlineSubsystemEOS*>(OnlineSub))
+		{
+			EOS_Connect_AddNotifyLoginStatusChangedOptions AddNotifyLoginStatusChangedOptions = { };
+			AddNotifyLoginStatusChangedOptions.ApiVersion = EOS_CONNECT_ADDNOTIFYLOGINSTATUSCHANGED_API_LATEST;
+			EOS_Connect_AddNotifyLoginStatusChanged(EOSRef->ConnectHandle, &AddNotifyLoginStatusChangedOptions, this, [] (const EOS_Connect_LoginStatusChangedCallbackInfo* Data)
+			{
+				UEIK_ConnectSubsystem* ConnectSubsystem = static_cast<UEIK_ConnectSubsystem*>(Data->ClientData);
+				if(ConnectSubsystem)
+				{
+					ConnectSubsystem->OnLoginStatusChanged.ExecuteIfBound(Data->LocalUserId, static_cast<EIK_ELoginStatus>(Data->CurrentStatus));
+				}
+			});
+			return;
+		}
+	}
+	UE_LOG(LogEIK, Error, TEXT("Failed to add notify login status changed either OnlineSubsystem is not valid or EOSRef is not valid."));
+}
+
+void UEIK_ConnectSubsystem::EIK_Connect_CopyIdToken(FEIK_ProductUserId LocalUserId, TEnumAsByte<EEIK_Result>& Result,
+	FString& OutIdToken)
+{
+	if(	IOnlineSubsystem* OnlineSub = IOnlineSubsystem::Get("EIK"))
+	{
+		if (FOnlineSubsystemEOS* EOSRef = static_cast<FOnlineSubsystemEOS*>(OnlineSub))
+		{
+			EOS_Connect_IdToken* IdToken = nullptr;
+			EOS_Connect_CopyIdTokenOptions CopyIdTokenOptions = { };
+			CopyIdTokenOptions.ApiVersion = EOS_CONNECT_COPYIDTOKEN_API_LATEST;
+			CopyIdTokenOptions.LocalUserId = LocalUserId.ProductUserId_FromString();
+			auto ResultVal = EOS_Connect_CopyIdToken(EOSRef->ConnectHandle, &CopyIdTokenOptions, &IdToken);
+			Result = static_cast<EEIK_Result>(ResultVal);
+			if(IdToken)
+			{
+				OutIdToken = IdToken->JsonWebToken;
+			}
+			return;
+		}
+	}
+	UE_LOG(LogEIK, Error, TEXT("Failed to copy id token either OnlineSubsystem is not valid or EOSRef is not valid."));
+}
---
+++ Source/OnlineSubsystemEIK/SdkFunctions/ConnectInterface/EIK_ConnectSubsystem.h
@@ -0,0 +1,35 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "OnlineSubsystemEIK/SdkFunctions/EIK_SharedFunctionFile.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "EIK_ConnectSubsystem.generated.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogEIK, Log, All);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FOnAuthExpirationCallback, FEIK_ProductUserId, LocalUserId);
+DECLARE_DYNAMIC_DELEGATE_TwoParams(FOnLoginStatusChangedCallback, FEIK_ProductUserId, LocalUserId, TEnumAsByte<EIK_ELoginStatus>, LoginStatus);
+
+UCLASS()
+class ONLINESUBSYSTEMEIK_API UEIK_ConnectSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+
+	FOnAuthExpirationCallback OnAuthExpiration;
+	FOnLoginStatusChangedCallback OnLoginStatusChanged;
+
+	//Register to receive upcoming authentication expiration notifications. Notification is approximately 10 minutes prior to expiration. Call EOS_Connect_Login again with valid third party credentials to refresh access.
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit | SDK Functions | Connect Interface", DisplayName="EOS_Connect_AddNotifyAuthExpiration")
+	void EIK_Connect_AddNotifyAuthExpiration(const FOnAuthExpirationCallback& Callback);
+
+	//Register to receive user login status updates.
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit | SDK Functions | Connect Interface", DisplayName="EOS_Connect_AddNotifyLoginStatusChanged")
+	void EIK_Connect_AddNotifyLoginStatusChanged(const FOnLoginStatusChangedCallback& Callback);
+
+	//Fetches an ID token for a Product User ID.
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit | SDK Functions | Connect Interface", DisplayName="EOS_Connect_CopyIdToken")
+	void EIK_Connect_CopyIdToken(FEIK_ProductUserId LocalUserId, TEnumAsByte<EEIK_Result>& Result, FString& OutIdToken);
+};
