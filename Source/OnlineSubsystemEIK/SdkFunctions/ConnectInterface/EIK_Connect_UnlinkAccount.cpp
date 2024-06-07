64085698c812fb00bb191d9c6c389f1b8814e040
Betide Studio <codingwithdevesh@gmail.com>
2024-06-08 00:56:05 +0530

    EOS_Connect_UnlinkAccount added

---
+++ Source/OnlineSubsystemEIK/SdkFunctions/ConnectInterface/EIK_Connect_UnlinkAccount.cpp
@@ -0,0 +1,50 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+
+#include "EIK_Connect_UnlinkAccount.h"
+
+#include "EIK_ConnectSubsystem.h"
+#include "OnlineSubsystemEOS.h"
+#include "Async/Async.h"
+
+UEIK_Connect_UnlinkAccount* UEIK_Connect_UnlinkAccount::EIK_Connect_UnlinkAccount(FEIK_ProductUserId LocalUserId)
+{
+	UEIK_Connect_UnlinkAccount* Node = NewObject<UEIK_Connect_UnlinkAccount>();
+	Node->Var_LocalUserId = LocalUserId;
+	return Node;
+}
+
+void UEIK_Connect_UnlinkAccount::OnUnlinkAccountCallback(const EOS_Connect_UnlinkAccountCallbackInfo* Data)
+{
+	if(UEIK_Connect_UnlinkAccount* Proxy = static_cast<UEIK_Connect_UnlinkAccount*>(Data->ClientData))
+	{
+		AsyncTask(ENamedThreads::GameThread, [Proxy, Data]()
+		{
+			Proxy->OnCallback.Broadcast(FEIK_ProductUserId(Data->LocalUserId), static_cast<EEIK_Result>(Data->ResultCode));
+		});
+		Proxy->SetReadyToDestroy();
+		Proxy->MarkAsGarbage();
+	}
+}
+
+void UEIK_Connect_UnlinkAccount::Activate()
+{
+	Super::Activate();
+	if(IOnlineSubsystem* OnlineSub = IOnlineSubsystem::Get("EIK"))
+	{
+		if(FOnlineSubsystemEOS* EOSRef = static_cast<FOnlineSubsystemEOS*>(OnlineSub))
+		{
+			EOS_Connect_UnlinkAccountOptions UnlinkAccountOptions = {};
+			UnlinkAccountOptions.ApiVersion = EOS_CONNECT_UNLINKACCOUNT_API_LATEST;
+			UnlinkAccountOptions.LocalUserId = Var_LocalUserId.ProductUserId_FromString();
+			EOS_Connect_UnlinkAccount(EOSRef->ConnectHandle, &UnlinkAccountOptions, this, &UEIK_Connect_UnlinkAccount::OnUnlinkAccountCallback);
+			return;
+		}
+	}
+	UE_LOG(LogEIK, Error, TEXT("Failed to unlink account either OnlineSubsystem is not valid or EOSRef is not valid."));
+	OnCallback.Broadcast(FEIK_ProductUserId(), EEIK_Result::EOS_ServiceFailure);
+	SetReadyToDestroy();
+	MarkAsGarbage();
+}
+
+
