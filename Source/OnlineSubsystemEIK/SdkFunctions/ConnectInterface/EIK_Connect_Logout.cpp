a4740272577d2429b234ca6b1f325a9161acd256
Betide Studio <codingwithdevesh@gmail.com>
2024-06-08 00:17:05 +0530

    EOS_Connect_Logout added

---
+++ Source/OnlineSubsystemEIK/SdkFunctions/ConnectInterface/EIK_Connect_Logout.cpp
@@ -0,0 +1,46 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+
+#include "EIK_Connect_Logout.h"
+
+#include "EIK_ConnectSubsystem.h"
+#include "OnlineSubsystemEOS.h"
+#include "Async/Async.h"
+
+UEIK_Connect_Logout* UEIK_Connect_Logout::EIK_Connect_Logout(FEIK_ProductUserId ProductUserId)
+{
+	UEIK_Connect_Logout* Proxy = NewObject<UEIK_Connect_Logout>();
+	Proxy->Var_ProductUserId = ProductUserId;
+	return Proxy;
+}
+
+void UEIK_Connect_Logout::OnLogoutCallback(const EOS_Connect_LogoutCallbackInfo* Data)
+{
+	if(UEIK_Connect_Logout* Proxy = static_cast<UEIK_Connect_Logout*>(Data->ClientData))
+	{
+		AsyncTask(ENamedThreads::GameThread, [Proxy, Data]()
+		{
+			Proxy->OnCallback.Broadcast(Proxy->Var_ProductUserId);
+		});
+		Proxy->SetReadyToDestroy();
+		Proxy->MarkAsGarbage();
+	}
+}
+
+void UEIK_Connect_Logout::Activate()
+{
+	Super::Activate();
+	if(	IOnlineSubsystem* OnlineSub = IOnlineSubsystem::Get("EIK"))
+	{
+		if (FOnlineSubsystemEOS* EOSRef = static_cast<FOnlineSubsystemEOS*>(OnlineSub))
+		{
+			EOS_Connect_LogoutOptions LogoutOptions = { };
+			LogoutOptions.ApiVersion = EOS_CONNECT_LOGOUT_API_LATEST;
+			LogoutOptions.LocalUserId = Var_ProductUserId.ProductUserId_FromString();
+			EOS_Connect_Logout(EOSRef->ConnectHandle, &LogoutOptions, this, &UEIK_Connect_Logout::OnLogoutCallback);
+			return;
+		}
+	}
+	UE_LOG(LogEIK, Error, TEXT("Failed to logout user either OnlineSubsystem is not valid or EOSRef is not valid."));
+	OnCallback.Broadcast(FEIK_ProductUserId());
+};
---
+++ Source/OnlineSubsystemEIK/SdkFunctions/ConnectInterface/EIK_Connect_Logout.h
@@ -0,0 +1,32 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "OnlineSubsystemEIK/SdkFunctions/EIK_SharedFunctionFile.h"
+#include "EIK_Connect_Logout.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FEIK_Connect_Logout_Delegate, const FEIK_ProductUserId&, ProductUserId);
+
+UCLASS()
+class ONLINESUBSYSTEMEIK_API UEIK_Connect_Logout : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+
+	//Logout a currently logged in user. NOTE: Access tokens for Product User IDs cannot be revoked. This operation really just cleans up state for the Product User ID and locally discards any associated access token.
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit | SDK Functions | Connect Interface", DisplayName="EOS_Connect_Logout")
+	static UEIK_Connect_Logout* EIK_Connect_Logout(FEIK_ProductUserId ProductUserId);
+	
+	UPROPERTY(BlueprintAssignable, Category = "EOS Integration Kit")
+	FEIK_Connect_Logout_Delegate OnCallback;
+
+	static void OnLogoutCallback(const EOS_Connect_LogoutCallbackInfo* Data);
+
+private:
+	FEIK_ProductUserId Var_ProductUserId;
+	virtual void Activate() override;
+	
+};
