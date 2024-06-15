9d4e454d67b38e515a27a98a75dcc315d36b9a4e
Betide Studio <codingwithdevesh@gmail.com>
2024-06-15 23:50:04 +0530

    EOS_Auth_QueryIdToken added

---
+++ Source/OnlineSubsystemEIK/SdkFunctions/AuthInterface/EIK_Auth_QueryIdToken.cpp
@@ -0,0 +1,52 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+
+#include "EIK_Auth_QueryIdToken.h"
+
+#include "OnlineSubsystemEOS.h"
+#include "Async/Async.h"
+#include "OnlineSubsystemEIK/SdkFunctions/ConnectInterface/EIK_ConnectSubsystem.h"
+
+UEIK_Auth_QueryIdToken* UEIK_Auth_QueryIdToken::QueryIdToken(FEIK_EpicAccountId LocalUserId,
+	FEIK_EpicAccountId TargetAccountId)
+{
+	UEIK_Auth_QueryIdToken* Node = NewObject<UEIK_Auth_QueryIdToken>();
+	Node->Var_LocalUserId = LocalUserId;
+	Node->Var_TargetAccountId = TargetAccountId;
+	return Node;
+}
+
+void UEIK_Auth_QueryIdToken::OnQueryIdTokenCallback(const EOS_Auth_QueryIdTokenCallbackInfo* Data)
+{
+	UEIK_Auth_QueryIdToken* Node = static_cast<UEIK_Auth_QueryIdToken*>(Data->ClientData);
+	if(Node)
+	{
+		AsyncTask(ENamedThreads::GameThread, [Node, Data]()
+		{
+			Node->OnCallback.Broadcast(static_cast<EEIK_Result>(Data->ResultCode), Data->LocalUserId, Data->TargetAccountId);
+		});
+		Node->SetReadyToDestroy();
+		Node->MarkAsGarbage();
+	}
+}
+
+void UEIK_Auth_QueryIdToken::Activate()
+{
+	Super::Activate();
+	if(	IOnlineSubsystem* OnlineSub = IOnlineSubsystem::Get("EIK"))
+	{
+		if (FOnlineSubsystemEOS* EOSRef = static_cast<FOnlineSubsystemEOS*>(OnlineSub))
+		{
+			EOS_Auth_QueryIdTokenOptions Options = {};
+			Options.ApiVersion = EOS_AUTH_QUERYIDTOKEN_API_LATEST;
+			Options.LocalUserId = Var_LocalUserId.EpicAccountId_FromString();
+			Options.TargetAccountId = Var_TargetAccountId.EpicAccountId_FromString();
+			EOS_Auth_QueryIdToken(EOSRef->AuthHandle, &Options, this, &UEIK_Auth_QueryIdToken::OnQueryIdTokenCallback);
+			return;
+		}
+	}
+	UE_LOG(LogEIK, Error, TEXT("Failed to query id token either OnlineSubsystem is not valid or EOSRef is not valid."));
+	OnCallback.Broadcast(EEIK_Result::EOS_NotFound, Var_LocalUserId, Var_TargetAccountId);
+	SetReadyToDestroy();
+	MarkAsGarbage();
+}
---
+++ Source/OnlineSubsystemEIK/SdkFunctions/AuthInterface/EIK_Auth_QueryIdToken.h
@@ -0,0 +1,34 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+THIRD_PARTY_INCLUDES_START
+#include <eos_auth.h>
+THIRD_PARTY_INCLUDES_END
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "OnlineSubsystemEIK/SdkFunctions/EIK_SharedFunctionFile.h"
+#include "EIK_Auth_QueryIdToken.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FEIK_Auth_OnQueryIdTokenCallback, TEnumAsByte<EEIK_Result>, ResultCode, FEIK_EpicAccountId, LocalUserId, FEIK_EpicAccountId, TargetAccountId);
+
+UCLASS()
+class ONLINESUBSYSTEMEIK_API UEIK_Auth_QueryIdToken : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+
+	//Query the backend for an ID token that describes one of the merged account IDs of a local authenticated user. The ID token can be used to impersonate a merged account ID when communicating with online services. An ID token for the selected account ID of a locally authenticated user will always be readily available and does not need to be queried explicitly.
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit | SDK Functions | Auth Interface", DisplayName = "EOS_Auth_QueryIdToken")
+	static UEIK_Auth_QueryIdToken* QueryIdToken(FEIK_EpicAccountId LocalUserId, FEIK_EpicAccountId TargetAccountId);
+
+	UPROPERTY(BlueprintAssignable, Category = "EOS Integration Kit")
+	FEIK_Auth_OnQueryIdTokenCallback OnCallback;
+private:
+	FEIK_EpicAccountId Var_LocalUserId;
+	FEIK_EpicAccountId Var_TargetAccountId;
+	static void EOS_CALL OnQueryIdTokenCallback(const EOS_Auth_QueryIdTokenCallbackInfo* Data);
+	virtual void Activate() override;
+	
+};
