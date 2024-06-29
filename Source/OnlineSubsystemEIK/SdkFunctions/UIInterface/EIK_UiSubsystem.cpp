18e818491287550ef74524e6016aaaaf3e0cc741
Betide Studio <codingwithdevesh@gmail.com>
2024-06-30 02:49:30 +0530

    UI Interface added

---
+++ Source/OnlineSubsystemEIK/SdkFunctions/UIInterface/EIK_UiSubsystem.cpp
@@ -0,0 +1,179 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+
+#include "EIK_UiSubsystem.h"
+
+#include "OnlineSubsystemEIK/SdkFunctions/ConnectInterface/EIK_ConnectSubsystem.h"
+
+TEnumAsByte<EEIK_Result> UEIK_UiSubsystem::EIK_UI_AcknowledgeEventId(const FEIK_UI_EventId& UiEventId, const TEnumAsByte<EEIK_Result>& Result)
+{
+	if (IOnlineSubsystem* OnlineSub = IOnlineSubsystem::Get("EIK"))
+	{
+		if (FOnlineSubsystemEOS* EOSRef = static_cast<FOnlineSubsystemEOS*>(OnlineSub))
+		{
+			EOS_UI_AcknowledgeEventIdOptions Options;
+			Options.ApiVersion = EOS_UI_ACKNOWLEDGEEVENTID_API_LATEST;
+			Options.Result = static_cast<EOS_EResult>(Result.GetValue());
+			Options.UiEventId = UiEventId.Ref;
+			auto AcknowledgeResult = EOS_UI_AcknowledgeEventId(EOSRef->UIHandle, &Options);
+			return static_cast<EEIK_Result>(AcknowledgeResult);
+		}
+	}
+	UE_LOG(LogEIK, Error, TEXT("UEIK_UiSubsystem::EIK_UI_AcknowledgeEventId: Failed to get EOS subsystem"));
+	return EEIK_Result::EOS_NotFound;
+}
+
+FEIK_NotificationId UEIK_UiSubsystem::EIK_UI_AddNotifyDisplaySettingsUpdated(FEIK_OnDisplaySettingsUpdated Callback)
+{
+	if (IOnlineSubsystem* OnlineSub = IOnlineSubsystem::Get("EIK"))
+	{
+		if (FOnlineSubsystemEOS* EOSRef = static_cast<FOnlineSubsystemEOS*>(OnlineSub))
+		{
+			EOS_UI_AddNotifyDisplaySettingsUpdatedOptions Options;
+			Options.ApiVersion = EOS_UI_ADDNOTIFYDISPLAYSETTINGSUPDATED_API_LATEST;
+			auto NotificationId = EOS_UI_AddNotifyDisplaySettingsUpdated(EOSRef->UIHandle, &Options,this,[](const EOS_UI_OnDisplaySettingsUpdatedCallbackInfo* Data)
+			{
+				UEIK_UiSubsystem* UiSubsystem = static_cast<UEIK_UiSubsystem*>(Data->ClientData);
+				if (UiSubsystem)
+				{
+					bool bIsVisible = Data->bIsVisible == EOS_TRUE;
+					bool bIsExclusiveInput = Data->bIsExclusiveInput == EOS_TRUE;
+					UiSubsystem->OnDisplaySettingsUpdated.ExecuteIfBound(bIsVisible, bIsExclusiveInput);
+				}
+			});
+			return NotificationId;
+		}
+	}
+	UE_LOG(LogEIK, Error, TEXT("UEIK_UiSubsystem::EIK_UI_AddNotifyDisplaySettingsUpdated: Failed to get EOS subsystem"));
+	return FEIK_NotificationId();
+}
+
+FEIK_NotificationId UEIK_UiSubsystem::EIK_UI_AddNotifyMemoryMonitor(FEIK_OnMemoryMonitor Callback)
+{
+	if (IOnlineSubsystem* OnlineSub = IOnlineSubsystem::Get("EIK"))
+	{
+		if (FOnlineSubsystemEOS* EOSRef = static_cast<FOnlineSubsystemEOS*>(OnlineSub))
+		{
+			EOS_UI_AddNotifyMemoryMonitorOptions Options;
+			Options.ApiVersion = EOS_UI_ADDNOTIFYMEMORYMONITOR_API_LATEST;
+			auto NotificationId = EOS_UI_AddNotifyMemoryMonitor(EOSRef->UIHandle, &Options, this, [](const EOS_UI_MemoryMonitorCallbackInfo* Data)
+			{
+				UEIK_UiSubsystem* UiSubsystem = static_cast<UEIK_UiSubsystem*>(Data->ClientData);
+				if (UiSubsystem)
+				{
+					UiSubsystem->OnMemoryMonitor.ExecuteIfBound();
+				}
+			});
+			return NotificationId;
+		}
+	}
+	UE_LOG(LogEIK, Error, TEXT("UEIK_UiSubsystem::EIK_UI_AddNotifyMemoryMonitor: Failed to get EOS subsystem"));
+	return FEIK_NotificationId();
+}
+
+bool UEIK_UiSubsystem::EIK_UI_GetFriendsExclusiveInput(const FEIK_EpicAccountId& LocalUserId)
+{
+	if (IOnlineSubsystem* OnlineSub = IOnlineSubsystem::Get("EIK"))
+	{
+		if (FOnlineSubsystemEOS* EOSRef = static_cast<FOnlineSubsystemEOS*>(OnlineSub))
+		{
+			EOS_UI_GetFriendsExclusiveInputOptions Options;
+			Options.ApiVersion = EOS_UI_GETFRIENDSEXCLUSIVEINPUT_API_LATEST;
+			Options.LocalUserId = LocalUserId.Ref;
+			auto ExclusiveInput = EOS_UI_GetFriendsExclusiveInput(EOSRef->UIHandle, &Options);
+			return ExclusiveInput == EOS_TRUE;
+		}
+	}
+	UE_LOG(LogEIK, Error, TEXT("UEIK_UiSubsystem::EIK_UI_GetFriendsExclusiveInput: Failed to get EOS subsystem"));
+	return false;
+}
+
+bool UEIK_UiSubsystem::EIK_UI_GetFriendsVisible(const FEIK_EpicAccountId& LocalUserId)
+{
+	if (IOnlineSubsystem* OnlineSub = IOnlineSubsystem::Get("EIK"))
+	{
+		if (FOnlineSubsystemEOS* EOSRef = static_cast<FOnlineSubsystemEOS*>(OnlineSub))
+		{
+			EOS_UI_GetFriendsVisibleOptions Options;
+			Options.ApiVersion = EOS_UI_GETFRIENDSVISIBLE_API_LATEST;
+			Options.LocalUserId = LocalUserId.Ref;
+			auto FriendsVisible = EOS_UI_GetFriendsVisible(EOSRef->UIHandle, &Options);
+			return FriendsVisible == EOS_TRUE;
+		}
+	}
+	UE_LOG(LogEIK, Error, TEXT("UEIK_UiSubsystem::EIK_UI_GetFriendsVisible: Failed to get EOS subsystem"));
+	return false;
+}
+
+TEnumAsByte<EEIK_UI_ENotificationLocation> UEIK_UiSubsystem::EIK_UI_GetNotificationLocationPreference()
+{
+	if (IOnlineSubsystem* OnlineSub = IOnlineSubsystem::Get("EIK"))
+	{
+		if (FOnlineSubsystemEOS* EOSRef = static_cast<FOnlineSubsystemEOS*>(OnlineSub))
+		{
+			auto NotificationLocation = EOS_UI_GetNotificationLocationPreference(EOSRef->UIHandle);
+			return static_cast<EEIK_UI_ENotificationLocation>(NotificationLocation);
+		}
+	}
+	UE_LOG(LogEIK, Error, TEXT("UEIK_UiSubsystem::EIK_UI_GetNotificationLocationPreference: Failed to get EOS subsystem"));
+	return EEIK_UI_ENotificationLocation::EIK_UNL_TopLeft;
+}
+
+TEnumAsByte<EEIK_Result> UEIK_UiSubsystem::EIK_UI_PauseSocialOverlay(bool bIsPaused)
+{
+	if (IOnlineSubsystem* OnlineSub = IOnlineSubsystem::Get("EIK"))
+	{
+		if (FOnlineSubsystemEOS* EOSRef = static_cast<FOnlineSubsystemEOS*>(OnlineSub))
+		{
+			EOS_UI_PauseSocialOverlayOptions Options;
+			Options.ApiVersion = EOS_UI_PAUSESOCIALOVERLAY_API_LATEST;
+			Options.bIsPaused = bIsPaused;
+			auto PauseResult = EOS_UI_PauseSocialOverlay(EOSRef->UIHandle, &Options);
+			return static_cast<EEIK_Result>(PauseResult);
+		}
+	}
+	UE_LOG(LogEIK, Error, TEXT("UEIK_UiSubsystem::EIK_UI_PauseSocialOverlay: Failed to get EOS subsystem"));
+	return EEIK_Result::EOS_NotFound;
+}
+
+void UEIK_UiSubsystem::EIK_UI_RemoveNotifyDisplaySettingsUpdated(FEIK_NotificationId NotificationId)
+{
+	if (IOnlineSubsystem* OnlineSub = IOnlineSubsystem::Get("EIK"))
+	{
+		if (FOnlineSubsystemEOS* EOSRef = static_cast<FOnlineSubsystemEOS*>(OnlineSub))
+		{
+			EOS_UI_RemoveNotifyDisplaySettingsUpdated(EOSRef->UIHandle, NotificationId.EOS_NotificationId_FromInt64());
+		}
+	}
+}
+
+void UEIK_UiSubsystem::EIK_UI_RemoveNotifyMemoryMonitor(FEIK_NotificationId NotificationId)
+{
+	{
+		if (IOnlineSubsystem* OnlineSub = IOnlineSubsystem::Get("EIK"))
+		{
+			if (FOnlineSubsystemEOS* EOSRef = static_cast<FOnlineSubsystemEOS*>(OnlineSub))
+			{
+				EOS_UI_RemoveNotifyMemoryMonitor(EOSRef->UIHandle, NotificationId.EOS_NotificationId_FromInt64());
+			}
+		}
+	}
+}
+
+TEnumAsByte<EEIK_Result> UEIK_UiSubsystem::EIK_UI_SetDisplayPreference(
+	const EEIK_UI_ENotificationLocation& DisplayPreference)
+{
+	if (IOnlineSubsystem* OnlineSub = IOnlineSubsystem::Get("EIK"))
+	{
+		if (FOnlineSubsystemEOS* EOSRef = static_cast<FOnlineSubsystemEOS*>(OnlineSub))
+		{
+			EOS_UI_SetDisplayPreferenceOptions Options;
+			Options.ApiVersion = EOS_UI_SETDISPLAYPREFERENCE_API_LATEST;
+			Options.NotificationLocation = static_cast<EOS_UI_ENotificationLocation>(DisplayPreference);
+			auto SetDisplayResult = EOS_UI_SetDisplayPreference(EOSRef->UIHandle, &Options);
+			return static_cast<EEIK_Result>(SetDisplayResult);
+		}
+	}
+	UE_LOG(LogEIK, Error, TEXT("UEIK_UiSubsystem::EIK_UI_SetDisplayPreference: Failed to get EOS subsystem"));
+	return EEIK_Result::EOS_NotFound;
+}
---
+++ Source/OnlineSubsystemEIK/SdkFunctions/UIInterface/EIK_UiSubsystem.h
@@ -0,0 +1,65 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "OnlineSubsystemEOS.h"
+#include "OnlineSubsystemEIK/SdkFunctions/EIK_SharedFunctionFile.h"
+THIRD_PARTY_INCLUDES_START
+#include "eos_ui.h"
+#include "eos_ui_types.h"
+THIRD_PARTY_INCLUDES_END
+#include "EIK_UiSubsystem.generated.h"
+
+DECLARE_DYNAMIC_DELEGATE_TwoParams(FEIK_OnDisplaySettingsUpdated, bool, bIsVisible, bool, bIsExclusiveInput);
+DECLARE_DYNAMIC_DELEGATE(FEIK_OnMemoryMonitor);
+UCLASS()
+class ONLINESUBSYSTEMEIK_API UEIK_UiSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	//Lets the SDK know that the given UI event ID has been acknowledged and should be released. EOS_Success is returned if the UI event ID has been acknowledged. EOS_NotFound is returned if the UI event ID does not exist.
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit | SDK Functions | UI Interface", DisplayName="EOS_UI_AcknowledgeEventId")
+	static TEnumAsByte<EEIK_Result> EIK_UI_AcknowledgeEventId(const FEIK_UI_EventId& UiEventId, const TEnumAsByte<EEIK_Result>& Result);
+
+	FEIK_OnDisplaySettingsUpdated OnDisplaySettingsUpdated;
+	//Register to receive notifications when the overlay display settings are updated. Newly registered handlers will always be called the next tick with the current state.
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit | SDK Functions | UI Interface", DisplayName="EOS_UI_AddNotifyDisplaySettingsUpdated")
+	FEIK_NotificationId EIK_UI_AddNotifyDisplaySettingsUpdated(FEIK_OnDisplaySettingsUpdated Callback);
+
+	FEIK_OnMemoryMonitor OnMemoryMonitor;
+	//Register to receive notifications from the memory monitor. Newly registered handlers will always be called the next tick with the current state.
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit | SDK Functions | UI Interface", DisplayName="EOS_UI_AddNotifyMemoryMonitor")
+	FEIK_NotificationId EIK_UI_AddNotifyMemoryMonitor(FEIK_OnMemoryMonitor Callback);
+
+	//Gets the friends overlay exclusive input state.
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit | SDK Functions | UI Interface", DisplayName="EOS_UI_GetFriendsExclusiveInput")
+	static bool EIK_UI_GetFriendsExclusiveInput(const FEIK_EpicAccountId& LocalUserId);
+
+	//Gets the friends overlay visibility.
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit | SDK Functions | UI Interface", DisplayName="EOS_UI_GetFriendsVisible")
+	static bool EIK_UI_GetFriendsVisible(const FEIK_EpicAccountId& LocalUserId);
+	
+	//Returns the current notification location display preference.
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit | SDK Functions | UI Interface", DisplayName="EOS_UI_GetNotificationLocationPreference")
+	static TEnumAsByte<EEIK_UI_ENotificationLocation> EIK_UI_GetNotificationLocationPreference();
+
+	//Sets the bIsPaused state of the overlay. While true then all notifications will be delayed until after the bIsPaused is false again. While true then the key and button events will not toggle the overlay. If the Overlay was visible before being paused then it will be hidden. If it is known that the Overlay should now be visible after being paused then it will be shown.
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit | SDK Functions | UI Interface", DisplayName="EOS_UI_PauseSocialOverlay")
+	TEnumAsByte<EEIK_Result> EIK_UI_PauseSocialOverlay(bool bIsPaused);
+
+	//Unregister from receiving notifications when the overlay display settings are updated.
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit | SDK Functions | UI Interface", DisplayName="EOS_UI_RemoveNotifyDisplaySettingsUpdated")
+	void EIK_UI_RemoveNotifyDisplaySettingsUpdated(FEIK_NotificationId NotificationId);
+
+	//Unregister from receiving notifications when the memory monitor posts a notification.
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit | SDK Functions | UI Interface", DisplayName="EOS_UI_RemoveNotifyMemoryMonitor")
+	void EIK_UI_RemoveNotifyMemoryMonitor(FEIK_NotificationId NotificationId);
+
+	
+	//Define any preferences for any display settings.
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit | SDK Functions | UI Interface", DisplayName="EOS_UI_SetDisplayPreference")
+	TEnumAsByte<EEIK_Result> EIK_UI_SetDisplayPreference(const EEIK_UI_ENotificationLocation& DisplayPreference);
+};
