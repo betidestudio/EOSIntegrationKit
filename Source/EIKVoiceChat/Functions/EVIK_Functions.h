d523c511cf0f70291b43176dd312faf3d384a436
Betide Studio <codingwithdevesh@gmail.com>
2024-07-14 16:42:54 +0530

    Fixed minor categories for EVIK

--- Source/EIKVoiceChat/Functions/EVIK_Functions.h
+++ Source/EIKVoiceChat/Functions/EVIK_Functions.h
@@ -28,7 +28,7 @@ public:
 	static void ConnectVoiceChat(const UObject* WorldContextObject, const FEIKResultDelegate& Result);
 
 	// Returns whether the EOS Voice Chat system is connected.
-	UFUNCTION(BlueprintPure, DisplayName = "Is EOS Voice Chat Connected?", Category = "EVIK", meta = (WorldContext = "WorldContextObject"))
+	UFUNCTION(BlueprintPure, DisplayName = "Is EOS Voice Chat Connected?", Category = "EOS Integration Kit|Voice Chat", meta = (WorldContext = "WorldContextObject"))
 	static bool IsVoiceChatConnected(const UObject* WorldContextObject);
 
     // Logs the player into the EOS Voice Chat system.
@@ -40,11 +40,11 @@ public:
 	static void LogoutEOSVoiceChat(const UObject* WorldContextObject, FString PlayerName, const FEIKResultDelegate& Result);
 
 	// Returns whether the EOS Voice Chat system is currently logging-in.
-	UFUNCTION(BlueprintPure, DisplayName = "Is EOS Voice Chat Logging-In?", Category = "EVIK", meta = (WorldContext = "WorldContextObject"))
+	UFUNCTION(BlueprintPure, DisplayName = "Is EOS Voice Chat Logging-In?", Category = "EOS Integration Kit|Voice Chat", meta = (WorldContext = "WorldContextObject"))
 	static bool IsEOSVoiceChatLoggingIn(const UObject* WorldContextObject);
 
 	// Returns whether the EOS Voice Chat system is logged-in.
-	UFUNCTION(BlueprintPure, DisplayName = "Is EOS Voice Chat Logged-In?", Category = "EVIK", meta = (WorldContext = "WorldContextObject"))
+	UFUNCTION(BlueprintPure, DisplayName = "Is EOS Voice Chat Logged-In?", Category = "EOS Integration Kit|Voice Chat", meta = (WorldContext = "WorldContextObject"))
 	static bool IsEOSVoiceChatLoggedIn(const UObject* WorldContextObject);
 
     // Gets the logged-in user's name for the EOS Voice Chat system.
