ab2193e6a6871dffc7a414f169854c55ba7ccadc
Amir-BK <Amir.BK@gmail.com>
2024-09-15 16:24:24 +0300

    Slight cleanup

--- Source/EIKVoiceChat/Private/EIKVoiceChatSynthComponent.cpp
+++ Source/EIKVoiceChat/Private/EIKVoiceChatSynthComponent.cpp
@@ -6,30 +6,32 @@
 bool UEIKVoiceChatSynthComponent::Init(int32& SampleRate)
 {
 	NumChannels = 1;
-
-#if SYNTHCOMPONENT_EX_OSCILATOR_ENABLED
-	// Initialize the DSP objects
-	Osc.Init(SampleRate);
-	Osc.SetFrequency(440.0f);
-	Osc.Start();
-#endif // SYNTHCOMPONENT_EX_OSCILATOR_ENABLED
-
 	return true;
 }
 
 int32 UEIKVoiceChatSynthComponent::OnGenerateAudio(float* OutAudio, int32 NumSamples)
 {
+	//we perform some basic checks to ensure we have enough samples to stream, if not we return 0
 	if (bIsReadyToStream)
 	{
+		if (AvailableSamples < NumSamples)
+		{
+			bIsReadyToStream = false;
+			return 0;
+		}
+
 		for (int32 Sample = 0; Sample < NumSamples; ++Sample)
 		{
 			OutAudio[Sample] = Buffer[ReadIndex];
 			ReadIndex = Buffer.GetNextIndex(ReadIndex);
 		}
+
+		AvailableSamples -= NumSamples;
+
 	}
 	else
 	{
-		//VERY HACKY! This is NOT how we should actually be doing this. This is just a quick and dirty way to get the audio to play.
+
 		if (WriteIndex > MinLatencySamples)
 		{
 			bIsReadyToStream = true;
@@ -39,18 +41,6 @@ int32 UEIKVoiceChatSynthComponent::OnGenerateAudio(float* OutAudio, int32 NumSam
 
 	}
 
-
 	return NumSamples;
 }
 
-void UEIKVoiceChatSynthComponent::SetFrequency(const float InFrequencyHz)
-{
-#if SYNTHCOMPONENT_EX_OSCILATOR_ENABLED
-	// Use this protected base class method to push a lambda function which will safely execute in the audio render thread.
-	SynthCommand([this, InFrequencyHz]()
-	{
-		Osc.SetFrequency(InFrequencyHz);
-		Osc.Update();
-	});
-#endif // SYNTHCOMPONENT_EX_OSCILATOR_ENABLED
-}
--- Source/EIKVoiceChat/Public/EIKVoiceChatSynthComponent.h
+++ Source/EIKVoiceChat/Public/EIKVoiceChatSynthComponent.h
@@ -8,44 +8,37 @@
 #include "Containers/CircularBuffer.h"
 #include "EIKVoiceChatSynthComponent.generated.h"
 
-// ========================================================================
-// UEIKVoiceChatSynthComponent
-// Synth component class which implements USynthComponent
-// This is a simple hello-world type example which generates a sine-wave
-// tone using a DSP oscillator class and implements a single function to set
-// the frequency. To enable example:
-// 1. Ensure "SignalProcessing" is added to project's .Build.cs in PrivateDependencyModuleNames
-// 2. Enable macro below that includes code utilizing SignalProcessing Oscilator
-// ========================================================================
-
-#define SYNTHCOMPONENT_EX_OSCILATOR_ENABLED 1
+/**
+ * EIK Voice Chat synth Component is used by the EOSVoiceChat System, when attached to a player pawn it will stream audio from the EOSVoiceChatUser allowing the use of spatialization and other audio features
+ */
 
 UCLASS(ClassGroup = Synth, meta = (BlueprintSpawnableComponent))
 class EIKVOICECHAT_API UEIKVoiceChatSynthComponent : public USynthComponent
 {
 	GENERATED_BODY()
 
+	//the minimum amount of samples we accumulate before we start streaming, to ensure there is no starvation on the audio thread, if there's too much latency this can be reduced
 	constexpr static int32 MinLatencySamples = 2048;
 	bool bIsReadyToStream = false;
 
+	//The circular buffer and its read/write indices, 44100 is really largely an arbitrary number here, a rather larger buffer
 	TCircularBuffer<float> Buffer = TCircularBuffer<float>(44100);
 	uint32 WriteIndex = 0;
 	uint32 ReadIndex = 0;
 
 
-	
 	// Called when synth is created
 	virtual bool Init(int32& SampleRate) override;
 
 	// Called to generate more audio
 	virtual int32 OnGenerateAudio(float* OutAudio, int32 NumSamples) override;
 
-	// Sets the oscillator's frequency
-	UFUNCTION(BlueprintCallable, Category = "Synth|Components|Audio")
-	void SetFrequency(const float FrequencyHz = 440.0f);
 
-public:
+protected:
 
+	friend class FEOSVoiceChatUser;
+
+	// called by EOSVoiceChatUser to fill the buffer, as long as these are samples available these should be consumed via OnGenerateAudio
 	void WriteSamples(TArrayView<int16> Samples)
 	{
 		for (int16 Sample : Samples)
@@ -53,21 +46,10 @@ public:
 			Buffer[WriteIndex] = Sample / 32768.0f;
 			WriteIndex = Buffer.GetNextIndex(WriteIndex);
 		}
+
+		AvailableSamples += Samples.Num();
 	}
 
-	void WriteSamples(float* Samples, uint32 NumSamples)
-	{
-		for (uint32 i = 0; i < NumSamples; i++)
-		{
-			Buffer[WriteIndex] = Samples[i];
-			WriteIndex = Buffer.GetNextIndex(WriteIndex);
-		}
-	}
+	uint32 AvailableSamples = 0;
 
-
-protected:
-#if SYNTHCOMPONENT_EX_OSCILATOR_ENABLED
-	// A simple oscillator class. Can also generate Saw/Square/Tri/Noise.
-	Audio::FOsc Osc;
-#endif // SYNTHCOMPONENT_EX_OSCILATOR_ENABLED
 };
