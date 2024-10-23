94132baa50572c3108a274c877298bc379f9d366
Betide Studio <codingwithdevesh@gmail.com>
2024-10-24 02:39:18 +0530

    Remove Voice Participant API Added

---
+++ Source/EIKWeb/Voice/EIK_API_RemoveVoiceParticipant.cpp
@@ -0,0 +1,35 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+
+#include "EIK_API_RemoveVoiceParticipant.h"
+
+UEIK_API_RemoveVoiceParticipant* UEIK_API_RemoveVoiceParticipant::RemoveVoiceParticipant(FString Authorization,
+	FString DeploymentId, FString RoomId, FString ProductUserId)
+{
+	UEIK_API_RemoveVoiceParticipant* Proxy = NewObject<UEIK_API_RemoveVoiceParticipant>();
+	Proxy->Var_Authorization = Authorization;
+	Proxy->Var_DeploymentId = DeploymentId;
+	Proxy->Var_RoomId = RoomId;
+	Proxy->Var_ProductUserId = ProductUserId;
+	return Proxy;
+}
+
+void UEIK_API_RemoveVoiceParticipant::Activate()
+{
+	Super::Activate();
+	FString URL = FString::Printf(TEXT("%s/rtc/v1/%s/room/%s/participants/%s"), *APIEndpoint, *Var_DeploymentId, *Var_RoomId, *Var_ProductUserId);
+
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = FHttpModule::Get().CreateRequest();
+	HttpRequest->SetVerb(TEXT("DELETE"));
+	HttpRequest->SetURL(URL);
+	if (Var_Authorization.Contains("Bearer"))
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), Var_Authorization);
+	}
+	else
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), FString::Printf(TEXT("Bearer %s"), *Var_Authorization));
+	}
+	HttpRequest->OnProcessRequestComplete().BindUObject(this, &UEIK_API_RemoveVoiceParticipant::OnResponseReceived);
+	HttpRequest->ProcessRequest();
+}
---
+++ Source/EIKWeb/Voice/EIK_API_RemoveVoiceParticipant.h
@@ -0,0 +1,29 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EIK_BaseWebApi.h"
+#include "EIK_API_RemoveVoiceParticipant.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EIKWEB_API UEIK_API_RemoveVoiceParticipant : public UEIK_BaseWebApi
+{
+	GENERATED_BODY()
+	
+public:
+
+	//The removeParticipant request will remove a player from the voice room and revoke their room token, preventing them from reusing their existing token to join the same room again.
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit|Web")
+	static UEIK_API_RemoveVoiceParticipant* RemoveVoiceParticipant(FString Authorization, FString DeploymentId, FString RoomId, FString ProductUserId);
+
+private:
+	virtual void Activate() override;
+	FString Var_Authorization;
+	FString Var_DeploymentId;
+	FString Var_RoomId;
+	FString Var_ProductUserId;
+};
