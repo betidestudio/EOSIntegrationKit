c43397b81986407ad01ebfc19850e622520366eb
Betide Studio <codingwithdevesh@gmail.com>
2024-10-24 02:38:36 +0530

    Web API Build.cs added

---
+++ Source/EIKWeb/EIKWeb.Build.cs
@@ -0,0 +1,27 @@
+﻿using UnrealBuildTool;
+
+public class EIKWeb : ModuleRules
+{
+    public EIKWeb(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+                "HTTP",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
---
+++ Source/EIKWeb/Private/EIKWeb.cpp
@@ -0,0 +1,17 @@
+﻿#include "EIKWeb.h"
+
+#define LOCTEXT_NAMESPACE "FEIKWebModule"
+
+void FEIKWebModule::StartupModule()
+{
+    
+}
+
+void FEIKWebModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FEIKWebModule, EIKWeb)
