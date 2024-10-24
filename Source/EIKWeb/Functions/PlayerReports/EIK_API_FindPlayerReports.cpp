dc27c7c76bdf3c80f977f58b03a4c0455fcedd53
Betide Studio <codingwithdevesh@gmail.com>
2024-10-24 15:15:12 +0530

    [API/Reports] Find Player Reports

---
+++ Source/EIKWeb/Functions/PlayerReports/EIK_API_FindPlayerReports.cpp
@@ -0,0 +1,68 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+
+#include "EIK_API_FindPlayerReports.h"
+
+UEIK_API_FindPlayerReports* UEIK_API_FindPlayerReports::FindPlayerReports(FString Authorization, FString DeploymentId,
+	FString ReportingPlayerId, FString ReportedPlayerId, int32 ReasonId, FString StartTime, FString EndTime,
+	bool bPagination, int32 Offset, int32 Limit, FString Order)
+{
+	UEIK_API_FindPlayerReports* Proxy = NewObject<UEIK_API_FindPlayerReports>();
+	Proxy->Var_Authorization = Authorization;
+	Proxy->Var_DeploymentId = DeploymentId;
+	Proxy->Var_ReportingPlayerId = ReportingPlayerId;
+	Proxy->Var_ReportedPlayerId = ReportedPlayerId;
+	Proxy->Var_ReasonId = ReasonId;
+	Proxy->Var_StartTime = StartTime;
+	Proxy->Var_EndTime = EndTime;
+	Proxy->Var_bPagination = bPagination;
+	Proxy->Var_Offset = Offset;
+	Proxy->Var_Limit = Limit;
+	Proxy->Var_Order = Order;
+	return Proxy;
+}
+
+void UEIK_API_FindPlayerReports::Activate()
+{
+	Super::Activate();
+	FString URL = FString::Printf(TEXT("%s/player-reports/v1/report/%s"), *APIEndpoint, *Var_DeploymentId);
+	FString QueryString = TEXT("?");
+	if (!Var_ReportingPlayerId.IsEmpty())
+	{
+		QueryString += FString::Printf(TEXT("reportingPlayerId=%s&"), *Var_ReportingPlayerId);
+	}
+	if (!Var_ReportedPlayerId.IsEmpty())
+	{
+		QueryString += FString::Printf(TEXT("reportedPlayerId=%s&"), *Var_ReportedPlayerId);
+	}
+	if (Var_ReasonId > 0)
+	{
+		QueryString += FString::Printf(TEXT("reasonId=%d&"), Var_ReasonId);
+	}
+	if (!Var_StartTime.IsEmpty())
+	{
+		QueryString += FString::Printf(TEXT("startTime=%s&"), *Var_StartTime);
+	}
+	if (!Var_EndTime.IsEmpty())
+	{
+		QueryString += FString::Printf(TEXT("endTime=%s&"), *Var_EndTime);
+	}
+	if (Var_bPagination)
+	{
+		QueryString += FString::Printf(TEXT("offset=%d&limit=%d&order=%s"), Var_Offset, Var_Limit, *Var_Order);
+	}
+	URL += QueryString;
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = FHttpModule::Get().CreateRequest();
+	HttpRequest->SetVerb(TEXT("GET"));
+	HttpRequest->SetURL(URL);
+	if (Var_Authorization.Contains("Bearer"))
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), Var_Authorization);
+	}
+	else
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), FString::Printf(TEXT("Bearer %s"), *Var_Authorization));
+	}
+	HttpRequest->OnProcessRequestComplete().BindUObject(this, &UEIK_API_FindPlayerReports::OnResponseReceived);
+	HttpRequest->ProcessRequest();
+}
---
+++ Source/EIKWeb/Functions/PlayerReports/EIK_API_FindPlayerReports.h
@@ -0,0 +1,36 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EIK_BaseWebApi.h"
+#include "EIK_API_FindPlayerReports.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EIKWEB_API UEIK_API_FindPlayerReports : public UEIK_BaseWebApi
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit|Web API|Player Reports")
+	static UEIK_API_FindPlayerReports* FindPlayerReports(FString Authorization, FString DeploymentId, FString ReportingPlayerId, FString ReportedPlayerId, int32 ReasonId, FString StartTime, FString EndTime, bool bPagination, int32 Offset = 0, int32 Limit = 50, FString Order = "time:desc");
+
+private:
+	
+	virtual void Activate() override;
+	FString Var_Authorization;
+	FString Var_DeploymentId;
+	FString Var_ReportingPlayerId;
+	FString Var_ReportedPlayerId;
+	int32 Var_ReasonId;
+	FString Var_StartTime;
+	FString Var_EndTime;
+	bool Var_bPagination;
+	int32 Var_Offset;
+	int32 Var_Limit;
+	FString Var_Order;
+};
