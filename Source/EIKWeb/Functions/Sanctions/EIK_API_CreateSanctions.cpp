cb4b6c5d2f34161cf18de5960302d204d520713c
Betide Studio <codingwithdevesh@gmail.com>
2024-10-24 14:54:49 +0530

    [API/Sanctions] Create Sanctions

---
+++ Source/EIKWeb/Functions/Sanctions/EIK_API_CreateSanctions.cpp
@@ -0,0 +1,99 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+
+#include "EIK_API_CreateSanctions.h"
+
+#include "JsonObjectConverter.h"
+
+UEIK_API_CreateSanctions* UEIK_API_CreateSanctions::CreateSanctions(FString Authorization, FString DeploymentId,
+                                                                    TArray<FEIK_SanctionPostPayload> Sanctions)
+{
+	UEIK_API_CreateSanctions* Proxy = NewObject<UEIK_API_CreateSanctions>();
+	Proxy->Var_Authorization = Authorization;
+	Proxy->Var_DeploymentId = DeploymentId;
+	Proxy->Var_Sanctions = Sanctions;
+	return Proxy;
+}
+
+void UEIK_API_CreateSanctions::Activate()
+{
+	Super::Activate();
+	FString URL = FString::Printf(TEXT("%s/sanctions/v1/%s/sanctions"), *APIEndpoint, *Var_DeploymentId);
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = FHttpModule::Get().CreateRequest();
+	HttpRequest->SetVerb(TEXT("POST"));
+	HttpRequest->SetURL(URL);
+	HttpRequest->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
+	if (Var_Authorization.Contains("Bearer"))
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), Var_Authorization);
+	}
+	else
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), FString::Printf(TEXT("Bearer %s"), *Var_Authorization));
+	}
+	TArray<TSharedPtr<FJsonValue>> SanctionsJson;
+	TSharedPtr<FJsonObject> RequestBodyJson = MakeShareable(new FJsonObject);
+	if(Var_Sanctions.Num() > 0)
+	{
+		for (auto Sanction : Var_Sanctions)
+		{
+			TSharedPtr<FJsonObject> SanctionJson = MakeShareable(new FJsonObject);
+			if(!Sanction.ProductUserId.IsEmpty())
+			{
+				SanctionJson->SetStringField(TEXT("productUserId"), Sanction.ProductUserId);
+			}
+			if(!Sanction.Action.IsEmpty())
+			{
+				SanctionJson->SetStringField(TEXT("action"), Sanction.Action);
+			}
+			if(!Sanction.Justification.IsEmpty())
+			{
+				SanctionJson->SetStringField(TEXT("justification"), Sanction.Justification);
+			}
+			if(!Sanction.Source.IsEmpty())
+			{
+				SanctionJson->SetStringField(TEXT("source"), Sanction.Source);
+			}
+			if(!Sanction.Tags.IsEmpty())
+			{
+				TArray<TSharedPtr<FJsonValue>> TagsJson;
+				for (auto Tag : Sanction.Tags)
+				{
+					TagsJson.Add(MakeShareable(new FJsonValueString(Tag)));
+				}
+				SanctionJson->SetArrayField(TEXT("tags"), TagsJson);
+			}
+			SanctionJson->SetBoolField(TEXT("pending"), Sanction.bPending);
+			if(!Sanction.Metadata.IsEmpty())
+			{
+				//Arbitrary metadata key/value pairs associated with this sanction
+				TSharedPtr<FJsonObject> MetadataJson = MakeShareable(new FJsonObject);
+				for (auto Meta : Sanction.Metadata)
+				{
+					MetadataJson->SetStringField(Meta.Key, Meta.Value);
+				}
+				SanctionJson->SetObjectField(TEXT("metadata"), MetadataJson);
+			}
+			if(!Sanction.DisplayName.IsEmpty())
+			{
+				SanctionJson->SetStringField(TEXT("displayName"), Sanction.DisplayName);
+			}
+			if(!Sanction.IdentityProvider.IsEmpty())
+			{
+				SanctionJson->SetStringField(TEXT("identityProvider"), Sanction.IdentityProvider);
+			}
+			if(!Sanction.AccountId.IsEmpty())
+			{
+				SanctionJson->SetStringField(TEXT("accountId"), Sanction.AccountId);
+			}
+			SanctionJson->SetNumberField(TEXT("duration"), Sanction.Duration);
+			SanctionsJson.Add(MakeShareable(new FJsonValueObject(SanctionJson)));
+		}
+	}
+	FString RequestBody;
+	TSharedRef<TJsonWriter<TCHAR>> JsonWriter = TJsonWriterFactory<>::Create(&RequestBody);
+	FJsonSerializer::Serialize(SanctionsJson, JsonWriter);
+	HttpRequest->SetContentAsString(RequestBody);
+	HttpRequest->OnProcessRequestComplete().BindUObject(this, &UEIK_API_CreateSanctions::OnResponseReceived);
+	HttpRequest->ProcessRequest();
+}
---
+++ Source/EIKWeb/Functions/Sanctions/EIK_API_CreateSanctions.h
@@ -0,0 +1,89 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EIK_BaseWebApi.h"
+#include "EIK_API_CreateSanctions.generated.h"
+
+USTRUCT(BlueprintType)
+struct FEIK_SanctionPostPayload
+{
+	GENERATED_BODY()
+
+	//Sanctioned user’s EOS ProductUserId
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EOS Integration Kit|Web")
+	FString ProductUserId;
+
+	//Action string associated with this sanction. Format: [a-zA-Z0-9_-]+. Min length: 1. Max length: 64.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EOS Integration Kit|Web")
+	FString Action;
+
+	//Justification string associated with this sanction. Min length: 1 Max length: 2048
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EOS Integration Kit|Web")
+	FString Justification;
+
+	//Source which created this sanction, e.g. developer-portal. Format: [a-zA-Z0-9_-]+. Min length: 2. Max length: 64.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EOS Integration Kit|Web")
+	FString Source;
+
+	//List of tags associated with this sanction. Items are case insensitive and unique. Item format: [a-zA-Z0-9_-]+. Item max length: 16
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EOS Integration Kit|Web")
+	TArray<FString> Tags;
+
+	//True if this sanction is currently pending
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EOS Integration Kit|Web")
+	bool bPending;
+
+	//Arbitrary metadata key/value pairs associated with this sanction
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EOS Integration Kit|Web")
+	TMap<FString, FString> Metadata;
+
+	//Display name of sanctioned user. Max length: 64
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EOS Integration Kit|Web")
+	FString DisplayName;
+
+	//Identity provider that the sanctioned user authenticated with. Max length: 64
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EOS Integration Kit|Web")
+	FString IdentityProvider;
+
+	//Sanctioned user's account ID with the specified identityProvider. Max length: 64
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EOS Integration Kit|Web")
+	FString AccountId;
+
+	//The length of the sanction in seconds
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EOS Integration Kit|Web")
+	int32 Duration = 0;
+
+	FEIK_SanctionPostPayload()
+	{
+		ProductUserId = "";
+		Action = "";
+		Justification = "";
+		Source = "";
+		Tags = TArray<FString>();
+		bPending = false;
+		Metadata = TMap<FString, FString>();
+		DisplayName = "";
+		IdentityProvider = "";
+		AccountId = "";
+		Duration = 0;
+	}
+	
+};
+UCLASS()
+class EIKWEB_API UEIK_API_CreateSanctions : public UEIK_BaseWebApi
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit|Web")
+	static UEIK_API_CreateSanctions* CreateSanctions(FString Authorization, FString DeploymentId, TArray<FEIK_SanctionPostPayload> Sanctions);
+
+private:
+	virtual void Activate() override;
+	FString Var_Authorization;
+	FString Var_DeploymentId;
+	TArray<FEIK_SanctionPostPayload> Var_Sanctions;
+};
