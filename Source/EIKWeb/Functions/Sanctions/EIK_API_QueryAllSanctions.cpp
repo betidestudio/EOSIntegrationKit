76ecda3df2b63381d7d1549b6052ff8dfe9dcbab
Betide Studio <codingwithdevesh@gmail.com>
2024-10-24 14:52:39 +0530

    [API/Sanctions] Query All Sanctions

---
+++ Source/EIKWeb/Functions/Sanctions/EIK_API_QueryAllSanctions.cpp
@@ -0,0 +1,34 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+
+#include "EIK_API_QueryAllSanctions.h"
+
+UEIK_API_QueryAllSanctions* UEIK_API_QueryAllSanctions::QueryAllSanctions(FString Authorization, FString DeploymentId,
+	int32 Limit, int32 Offset)
+{
+	UEIK_API_QueryAllSanctions* Proxy = NewObject<UEIK_API_QueryAllSanctions>();
+	Proxy->Var_Authorization = Authorization;
+	Proxy->Var_DeploymentId = DeploymentId;
+	Proxy->Var_Limit = Limit;
+	Proxy->Var_Offset = Offset;
+	return Proxy;
+}
+
+void UEIK_API_QueryAllSanctions::Activate()
+{
+	Super::Activate();
+	FString URL = FString::Printf(TEXT("%s/sanctions/v1/%s/sanctions?limit=%d&offset=%d"), *APIEndpoint, *Var_DeploymentId, Var_Limit, Var_Offset);
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = FHttpModule::Get().CreateRequest();
+	HttpRequest->SetVerb(TEXT("GET"));
+	HttpRequest->SetURL(URL);
+	if (Var_Authorization.Contains("Bearer"))
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), Var_Authorization);
+	}
+	else
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), FString::Printf(TEXT("Bearer %s"), *Var_Authorization));
+	}
+	HttpRequest->OnProcessRequestComplete().BindUObject(this, &UEIK_API_QueryAllSanctions::OnResponseReceived);
+	HttpRequest->ProcessRequest();	
+}
---
+++ Source/EIKWeb/Functions/Sanctions/EIK_API_QueryAllSanctions.h
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EIK_BaseWebApi.h"
+#include "EIK_API_QueryAllSanctions.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EIKWEB_API UEIK_API_QueryAllSanctions : public UEIK_BaseWebApi
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit|Web")
+	static UEIK_API_QueryAllSanctions* QueryAllSanctions(FString Authorization, FString DeploymentId, int32 Limit = 100, int32 Offset = 0);
+
+private:
+	virtual void Activate() override;
+	FString Var_Authorization;
+	FString Var_DeploymentId;
+	int32 Var_Limit;
+	int32 Var_Offset;
+};
