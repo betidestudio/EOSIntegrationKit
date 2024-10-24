54cfa07b137b3422752ec47d8bb177de1d0b5282
Betide Studio <codingwithdevesh@gmail.com>
2024-10-24 14:52:17 +0530

    [API/Sanctions] Query Active Sanctions

---
+++ Source/EIKWeb/Functions/Sanctions/EIK_API_QueryActiveSanctions.cpp
@@ -0,0 +1,46 @@
+﻿// Copyright (c) 2024 Betide Studio. All Rights Reserved.
+
+
+#include "EIK_API_QueryActiveSanctions.h"
+
+UEIK_API_QueryActiveSanctions* UEIK_API_QueryActiveSanctions::QueryActiveSanctions(FString Authorization,
+	FString ProductUserId, TArray<FString> Action)
+{
+	UEIK_API_QueryActiveSanctions* Proxy = NewObject<UEIK_API_QueryActiveSanctions>();
+	Proxy->Var_Authorization = Authorization;
+	Proxy->Var_ProductUserId = ProductUserId;
+	Proxy->Var_Action = Action;
+	return Proxy;
+}
+
+void UEIK_API_QueryActiveSanctions::Activate()
+{
+	Super::Activate();
+	FString URL = FString::Printf(TEXT("%s/sanctions/v1/productUser/%s/active"), *APIEndpoint, *Var_ProductUserId);
+	if(Var_Action.Num() > 0)
+	{
+		FString ActionString = TEXT("action=");
+		for (int i = 0; i < Var_Action.Num(); i++)
+		{
+			ActionString += Var_Action[i];
+			if(i < Var_Action.Num() - 1)
+			{
+				ActionString += TEXT("&action=");
+			}
+		}
+		URL += FString::Printf(TEXT("?%s"), *ActionString);
+	}
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = FHttpModule::Get().CreateRequest();
+	HttpRequest->SetVerb(TEXT("GET"));
+	HttpRequest->SetURL(URL);
+	if (Var_Authorization.Contains("Bearer"))
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), Var_Authorization);
+	}
+	else
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), FString::Printf(TEXT("Bearer %s"), *Var_Authorization));
+	}
+	HttpRequest->OnProcessRequestComplete().BindUObject(this, &UEIK_API_QueryActiveSanctions::OnResponseReceived);
+	HttpRequest->ProcessRequest();
+}
---
+++ Source/EIKWeb/Functions/Sanctions/EIK_API_QueryActiveSanctions.h
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2024 Betide Studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EIK_BaseWebApi.h"
+#include "EIK_API_QueryActiveSanctions.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EIKWEB_API UEIK_API_QueryActiveSanctions : public UEIK_BaseWebApi
+{
+	GENERATED_BODY()
+
+public:
+
+	//The queryActiveSanctions request will return a list of all active sanctions for a given player.
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit|Web")
+	static UEIK_API_QueryActiveSanctions* QueryActiveSanctions(FString Authorization, FString ProductUserId, TArray<FString> Action);
+
+private:
+	virtual void Activate() override;
+	FString Var_Authorization;
+	FString Var_ProductUserId;
+	TArray<FString> Var_Action;
+};
