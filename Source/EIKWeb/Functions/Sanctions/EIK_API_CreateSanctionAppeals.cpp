49ac07faa3a3f1cef7090a1aede254b4bdcfb83b
Betide Studio <codingwithdevesh@gmail.com>
2024-10-24 14:51:47 +0530

    [API/Sanctions] Create Sanction Appeals

---
+++ Source/EIKWeb/Functions/Sanctions/EIK_API_CreateSanctionAppeals.cpp
@@ -0,0 +1,48 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+
+#include "EIK_API_CreateSanctionAppeals.h"
+
+UEIK_API_CreateSanctionAppeals* UEIK_API_CreateSanctionAppeals::CreateSanctionAppeals(FString Authorization,
+	FString DeploymentId, FString ReferenceId, int32 Reason)
+{
+	UEIK_API_CreateSanctionAppeals* Proxy = NewObject<UEIK_API_CreateSanctionAppeals>();
+	Proxy->Var_Authorization = Authorization;
+	Proxy->Var_DeploymentId = DeploymentId;
+	Proxy->Var_ReferenceId = ReferenceId;
+	Proxy->Var_Reason = Reason;
+	return Proxy;
+}
+
+void UEIK_API_CreateSanctionAppeals::Activate()
+{
+	Super::Activate();
+	FString URL = FString::Printf(TEXT("%s/sanctions/v1/%s/appeals"), *APIEndpoint, *Var_DeploymentId);
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = FHttpModule::Get().CreateRequest();
+	HttpRequest->SetVerb(TEXT("POST"));
+	HttpRequest->SetURL(URL);
+	HttpRequest->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
+	if (Var_Authorization.Contains("Bearer"))
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), Var_Authorization);
+	}
+	else
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), FString::Printf(TEXT("Bearer %s"), *Var_Authorization));
+	}
+	TSharedPtr<FJsonObject> RequestBodyJson = MakeShareable(new FJsonObject);
+	if (!Var_ReferenceId.IsEmpty())
+	{
+		RequestBodyJson->SetStringField(TEXT("referenceId"), Var_ReferenceId);
+	}
+	if (Var_Reason > 0)
+	{
+		RequestBodyJson->SetNumberField(TEXT("reason"), Var_Reason);
+	}
+	FString RequestBodyString;
+	TSharedRef<TJsonWriter<TCHAR>> JsonWriter = TJsonWriterFactory<>::Create(&RequestBodyString);
+	FJsonSerializer::Serialize(RequestBodyJson.ToSharedRef(), JsonWriter);
+	HttpRequest->SetContentAsString(RequestBodyString);
+	HttpRequest->OnProcessRequestComplete().BindUObject(this, &UEIK_API_CreateSanctionAppeals::OnResponseReceived);
+	HttpRequest->ProcessRequest();
+}
---
+++ Source/EIKWeb/Functions/Sanctions/EIK_API_CreateSanctionAppeals.h
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EIK_BaseWebApi.h"
+#include "EIK_API_CreateSanctionAppeals.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EIKWEB_API UEIK_API_CreateSanctionAppeals : public UEIK_BaseWebApi
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit|Web")
+	static UEIK_API_CreateSanctionAppeals* CreateSanctionAppeals(FString Authorization, FString DeploymentId, FString ReferenceId, int32 Reason);
+
+private:
+	virtual void Activate() override;
+	FString Var_Authorization;
+	FString Var_DeploymentId;
+	FString Var_ReferenceId;
+	int32 Var_Reason;
+};
