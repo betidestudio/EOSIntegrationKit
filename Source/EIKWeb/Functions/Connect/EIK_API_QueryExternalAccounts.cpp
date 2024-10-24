ef751ead6ca836efc2290f9de3eb35842d4706bf
Betide Studio <codingwithdevesh@gmail.com>
2024-10-24 19:56:57 +0530

    [API/Connect] Query External Accounts

---
+++ Source/EIKWeb/Functions/Connect/EIK_API_QueryExternalAccounts.cpp
@@ -0,0 +1,53 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+
+#include "EIK_API_QueryExternalAccounts.h"
+
+UEIK_API_QueryExternalAccounts* UEIK_API_QueryExternalAccounts::QueryExternalAccounts(FString Authorization,
+	TArray<FString> AccountId, FString IdentityProviderId, FString Environment)
+{
+	UEIK_API_QueryExternalAccounts* Proxy = NewObject<UEIK_API_QueryExternalAccounts>();
+	Proxy->Var_Authorization = Authorization;
+	Proxy->Var_AccountId = AccountId;
+	Proxy->Var_IdentityProviderId = IdentityProviderId;
+	Proxy->Var_Environment = Environment;
+	return Proxy;
+}
+
+void UEIK_API_QueryExternalAccounts::Activate()
+{
+	Super::Activate();
+	FString URL = FString::Printf(TEXT("%s/user/v1/accounts"), *APIEndpoint);
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = FHttpModule::Get().CreateRequest();
+	HttpRequest->SetVerb(TEXT("GET"));
+	HttpRequest->SetURL(URL);
+	HttpRequest->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
+	if (Var_Authorization.Contains("Bearer"))
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), Var_Authorization);
+	}
+	else
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), FString::Printf(TEXT("Bearer %s"), *Var_Authorization));
+	}
+	FString QueryString = TEXT("?");
+	if (Var_AccountId.Num() > 0)
+	{
+		for (auto AccountId : Var_AccountId)
+		{
+			QueryString += FString::Printf(TEXT("accountId=%s&"), *AccountId);
+		}
+	}
+	if (!Var_IdentityProviderId.IsEmpty())
+	{
+		QueryString += FString::Printf(TEXT("identityProviderId=%s&"), *Var_IdentityProviderId);
+	}
+	if (!Var_Environment.IsEmpty())
+	{
+		QueryString += FString::Printf(TEXT("environment=%s"), *Var_Environment);
+	}
+	URL += QueryString;
+	HttpRequest->OnProcessRequestComplete().BindUObject(this, &UEIK_API_QueryExternalAccounts::OnResponseReceived);
+	HttpRequest->ProcessRequest();
+}
+
---
+++ Source/EIKWeb/Functions/Connect/EIK_API_QueryExternalAccounts.h
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EIK_BaseWebApi.h"
+#include "EIK_API_QueryExternalAccounts.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EIKWEB_API UEIK_API_QueryExternalAccounts : public UEIK_BaseWebApi
+{
+	GENERATED_BODY()
+
+	//The request queryExternalAccountsForAnyUser returns associated Product User IDs from a list of external account IDs.
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit|Web API|Connect")
+	static UEIK_API_QueryExternalAccounts* QueryExternalAccounts(FString Authorization, TArray<FString> AccountId, FString IdentityProviderId, FString Environment);
+
+private:
+	
+	virtual void Activate() override;
+	FString Var_Authorization;
+	TArray<FString> Var_AccountId;
+	FString Var_IdentityProviderId;
+	FString Var_Environment;
+};
