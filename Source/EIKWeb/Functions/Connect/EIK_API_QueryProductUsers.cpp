d211293617ae23cc5ddef39d536069d06ee01a73
Betide Studio <codingwithdevesh@gmail.com>
2024-10-24 20:38:05 +0530

    [API/Connect] Query Product Users

---
+++ Source/EIKWeb/Functions/Connect/EIK_API_QueryProductUsers.cpp
@@ -0,0 +1,42 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+
+#include "EIK_API_QueryProductUsers.h"
+
+UEIK_API_QueryProductUsers* UEIK_API_QueryProductUsers::QueryProductUsers(FString Authorization,
+	TArray<FString> ProductUserIds)
+{
+	UEIK_API_QueryProductUsers* Node = NewObject<UEIK_API_QueryProductUsers>();
+	Node->Var_Authorization = Authorization;
+	Node->Var_ProductUserIds = ProductUserIds;
+	return Node;
+}
+
+void UEIK_API_QueryProductUsers::Activate()
+{
+	Super::Activate();
+	FString URL = FString::Printf(TEXT("%s/user/v1/product-users"), *APIEndpoint);
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = FHttpModule::Get().CreateRequest();
+	HttpRequest->SetVerb(TEXT("GET"));
+	HttpRequest->SetURL(URL);
+	HttpRequest->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
+	if (Var_Authorization.Contains("Bearer"))
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), Var_Authorization);
+	}
+	else
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), FString::Printf(TEXT("Bearer %s"), *Var_Authorization));
+	}
+	FString QueryString = TEXT("?");
+	if (Var_ProductUserIds.Num() > 0)
+	{
+		for (auto ProductUserId : Var_ProductUserIds)
+		{
+			QueryString += FString::Printf(TEXT("productUserId=%s&"), *ProductUserId);
+		}
+	}
+	URL += QueryString;
+	HttpRequest->OnProcessRequestComplete().BindUObject(this, &UEIK_API_QueryProductUsers::OnResponseReceived);
+	HttpRequest->ProcessRequest();
+}	
---
+++ Source/EIKWeb/Functions/Connect/EIK_API_QueryProductUsers.h
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EIK_BaseWebApi.h"
+#include "EIK_API_QueryProductUsers.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EIKWEB_API UEIK_API_QueryProductUsers : public UEIK_BaseWebApi
+{
+	GENERATED_BODY()
+
+public:
+
+	//The request queryProductUsersForAnyUser returns associated accounts from a list of Product User IDs.
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit|Web API|Connect")
+	static UEIK_API_QueryProductUsers* QueryProductUsers(FString Authorization, TArray<FString> ProductUserIds);
+
+private:
+	
+	virtual void Activate() override;
+	FString Var_Authorization;
+	TArray<FString> Var_ProductUserIds;
+};
