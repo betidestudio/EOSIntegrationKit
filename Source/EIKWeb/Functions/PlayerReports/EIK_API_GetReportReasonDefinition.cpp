3ef20e14912adce9913dec026bfe5dd22bbd3b1e
Betide Studio <codingwithdevesh@gmail.com>
2024-10-24 15:15:27 +0530

    [API/Reports] Get Report Reason Definition

---
+++ Source/EIKWeb/Functions/PlayerReports/EIK_API_GetReportReasonDefinition.cpp
@@ -0,0 +1,31 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+
+#include "EIK_API_GetReportReasonDefinition.h"
+
+UEIK_API_GetReportReasonDefinition* UEIK_API_GetReportReasonDefinition::GetReportReasonDefinition(FString Authorization)
+{
+	UEIK_API_GetReportReasonDefinition* Proxy = NewObject<UEIK_API_GetReportReasonDefinition>();
+	Proxy->Var_Authorization = Authorization;
+	return Proxy;
+}
+
+void UEIK_API_GetReportReasonDefinition::Activate()
+{
+	Super::Activate();
+	FString URL = FString::Printf(TEXT("%s/player-reports/v1/report/reason/definition"), *APIEndpoint);
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = FHttpModule::Get().CreateRequest();
+	HttpRequest->SetVerb(TEXT("GET"));
+	HttpRequest->SetURL(URL);
+	HttpRequest->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
+	if (Var_Authorization.Contains("Bearer"))
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), Var_Authorization);
+	}
+	else
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), FString::Printf(TEXT("Bearer %s"), *Var_Authorization));
+	}
+	HttpRequest->OnProcessRequestComplete().BindUObject(this, &UEIK_API_GetReportReasonDefinition::OnResponseReceived);
+	HttpRequest->ProcessRequest();
+}
---
+++ Source/EIKWeb/Functions/PlayerReports/EIK_API_GetReportReasonDefinition.h
@@ -0,0 +1,26 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EIK_BaseWebApi.h"
+#include "EIK_API_GetReportReasonDefinition.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EIKWEB_API UEIK_API_GetReportReasonDefinition : public UEIK_BaseWebApi
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit|Web API|Player Reports")
+	static UEIK_API_GetReportReasonDefinition* GetReportReasonDefinition(FString Authorization);
+
+private:
+	
+	virtual void Activate() override;
+	FString Var_Authorization;
+};
