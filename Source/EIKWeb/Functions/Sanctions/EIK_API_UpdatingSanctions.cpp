753f273df9d512532a6aab8c0ed18c7443aa9860
Betide Studio <codingwithdevesh@gmail.com>
2024-10-24 14:54:35 +0530

    [API/Sanctions] Update Sanctions

---
+++ Source/EIKWeb/Functions/Sanctions/EIK_API_UpdatingSanctions.cpp
@@ -0,0 +1,77 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+
+#include "EIK_API_UpdatingSanctions.h"
+
+UEIK_API_UpdatingSanctions* UEIK_API_UpdatingSanctions::UpdatingSanctions(FString Authorization, FString DeploymentId, TArray<FEIK_SanctionPatchPayload> SanctionPatchPayload)
+{
+	UEIK_API_UpdatingSanctions* Proxy = NewObject<UEIK_API_UpdatingSanctions>();
+	Proxy->Var_Authorization = Authorization;
+	Proxy->Var_DeploymentId = DeploymentId;
+	Proxy->Var_SanctionPatchPayload = SanctionPatchPayload;
+	return Proxy;
+}
+
+void UEIK_API_UpdatingSanctions::Activate()
+{
+	Super::Activate();
+	FString URL = FString::Printf(TEXT("%s/sanctions/v1/%s/sanctions"), *APIEndpoint, *Var_DeploymentId);
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = FHttpModule::Get().CreateRequest();
+	HttpRequest->SetVerb(TEXT("PATCH"));
+	HttpRequest->SetURL(URL);
+	HttpRequest->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
+	if (Var_Authorization.Contains("Bearer"))
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), Var_Authorization);
+	}
+	else
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), FString::Printf(TEXT("Bearer %s"), *Var_Authorization));
+	}
+	TSharedPtr<FJsonObject> RequestBodyJson = MakeShareable(new FJsonObject);
+	if (!Var_SanctionPatchPayload.IsEmpty())
+	{
+		TArray<TSharedPtr<FJsonValue>> SanctionsJson;
+		for (auto Sanction : Var_SanctionPatchPayload)
+		{
+			TSharedPtr<FJsonObject> SanctionJson = MakeShareable(new FJsonObject);
+			if (!Sanction.ReferenceId.IsEmpty())
+			{
+				SanctionJson->SetStringField(TEXT("referenceId"), Sanction.ReferenceId);
+			}
+			for(auto UpdateItem : Sanction.Updates)
+			{
+				if (!UpdateItem.Justification.IsEmpty())
+				{
+					SanctionJson->SetStringField(TEXT("justification"), UpdateItem.Justification);
+				}
+				if (!UpdateItem.Metadata.IsEmpty())
+				{
+					TSharedPtr<FJsonObject> MetadataJson = MakeShareable(new FJsonObject);
+					for (auto MetadataItem : UpdateItem.Metadata)
+					{
+						MetadataJson->SetStringField(MetadataItem.Key, MetadataItem.Value);
+					}
+					SanctionJson->SetObjectField(TEXT("metadata"), MetadataJson);
+				}
+				if (!UpdateItem.Tags.IsEmpty())
+				{
+					TArray<TSharedPtr<FJsonValue>> TagsJson;
+					for (auto Tag : UpdateItem.Tags)
+					{
+						TagsJson.Add(MakeShareable(new FJsonValueString(Tag)));
+					}
+					SanctionJson->SetArrayField(TEXT("tags"), TagsJson);
+				}
+			}
+			SanctionsJson.Add(MakeShareable(new FJsonValueObject(SanctionJson)));
+		}
+		RequestBodyJson->SetArrayField(TEXT("sanctions"), SanctionsJson);
+	}
+	FString RequestBody;
+	TSharedRef<TJsonWriter<TCHAR>> JsonWriter = TJsonWriterFactory<>::Create(&RequestBody);
+	FJsonSerializer::Serialize(RequestBodyJson.ToSharedRef(), JsonWriter);
+	HttpRequest->SetContentAsString(RequestBody);
+	HttpRequest->OnProcessRequestComplete().BindUObject(this, &UEIK_API_UpdatingSanctions::OnResponseReceived);
+	HttpRequest->ProcessRequest();
+}
---
+++ Source/EIKWeb/Functions/Sanctions/EIK_API_UpdatingSanctions.h
@@ -0,0 +1,56 @@
+﻿// Copyright (c) 2024 Betide Studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EIK_BaseWebApi.h"
+#include "EIK_API_UpdatingSanctions.generated.h"
+
+USTRUCT(BlueprintType)
+struct FEIK_UpdatableFields
+{
+	GENERATED_BODY()
+
+	//List of tags associated with this sanction. Items are case insensitive and unique. Item format: [a-zA-Z0-9_-]+. Item max length: 16
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EOS Integration Kit|Web")
+	TArray<FString> Tags;
+
+	//Arbitrary metadata key/value pairs associated with this sanction
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EOS Integration Kit|Web")
+	TMap<FString, FString> Metadata;
+
+	//Justification string associated with this sanction. Min length: 1 Max length: 2048
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EOS Integration Kit|Web")
+	FString Justification;
+	
+};
+
+USTRUCT(BlueprintType)
+struct FEIK_SanctionPatchPayload
+{
+	GENERATED_BODY()
+
+	//Unique identifier for this sanction	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EOS Integration Kit|Web")
+	FString ReferenceId;
+
+	//Fields to be updated and their new values.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EOS Integration Kit|Web")
+	TArray<FEIK_UpdatableFields> Updates;
+};
+UCLASS()
+class EIKWEB_API UEIK_API_UpdatingSanctions : public UEIK_BaseWebApi
+{
+	GENERATED_BODY()
+
+public:
+	
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit|Web")
+	static UEIK_API_UpdatingSanctions* UpdatingSanctions(FString Authorization, FString DeploymentId, TArray<FEIK_SanctionPatchPayload> SanctionPatchPayload);
+
+private:
+	virtual void Activate() override;
+	FString Var_Authorization;
+	FString Var_DeploymentId;
+	TArray<FEIK_SanctionPatchPayload> Var_SanctionPatchPayload;
+};
