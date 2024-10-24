fff9f875fcf63c3d2288210ed7f97c1ccc2fb1a3
Betide Studio <codingwithdevesh@gmail.com>
2024-10-24 14:53:04 +0530

    [API/Sanctions] Query All Sanctions for Player

---
+++ Source/EIKWeb/Functions/Sanctions/EIK_API_QueryAllSanctionsForPlayer.cpp
@@ -0,0 +1,35 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+
+#include "EIK_API_QueryAllSanctionsForPlayer.h"
+
+UEIK_API_QueryAllSanctionsForPlayer* UEIK_API_QueryAllSanctionsForPlayer::QueryAllSanctionsForPlayer(
+	FString Authorization, FString DeploymentId, FString ProductUserId, int32 Limit, int32 Offset)
+{
+	UEIK_API_QueryAllSanctionsForPlayer* Proxy = NewObject<UEIK_API_QueryAllSanctionsForPlayer>();
+	Proxy->Var_Authorization = Authorization;
+	Proxy->Var_DeploymentId = DeploymentId;
+	Proxy->Var_ProductUserId = ProductUserId;
+	Proxy->Var_Limit = Limit;
+	Proxy->Var_Offset = Offset;
+	return Proxy;
+}
+
+void UEIK_API_QueryAllSanctionsForPlayer::Activate()
+{
+	Super::Activate();
+	FString URL = FString::Printf(TEXT("%s/sanctions/v1/%s/users/%s?limit=%d&offset=%d"), *APIEndpoint, *Var_DeploymentId, *Var_ProductUserId, Var_Limit, Var_Offset);
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = FHttpModule::Get().CreateRequest();
+	HttpRequest->SetVerb(TEXT("GET"));
+	HttpRequest->SetURL(URL);
+	if (Var_Authorization.Contains("Bearer"))
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), Var_Authorization);
+	}
+	else
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), FString::Printf(TEXT("Bearer %s"), *Var_Authorization));
+	}
+	HttpRequest->OnProcessRequestComplete().BindUObject(this, &UEIK_API_QueryAllSanctionsForPlayer::OnResponseReceived);
+	HttpRequest->ProcessRequest();
+}
---
+++ Source/EIKWeb/Functions/Sanctions/EIK_API_QueryAllSanctionsForPlayer.h
@@ -0,0 +1,29 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EIK_BaseWebApi.h"
+#include "EIK_API_QueryAllSanctionsForPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EIKWEB_API UEIK_API_QueryAllSanctionsForPlayer : public UEIK_BaseWebApi
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit|Web")
+	static UEIK_API_QueryAllSanctionsForPlayer* QueryAllSanctionsForPlayer(FString Authorization, FString DeploymentId, FString ProductUserId, int32 Limit = 100, int32 Offset = 0);
+
+private:
+	virtual void Activate() override;
+	FString Var_Authorization;
+	FString Var_DeploymentId;
+	FString Var_ProductUserId;
+	int32 Var_Limit;
+	int32 Var_Offset;
+};
