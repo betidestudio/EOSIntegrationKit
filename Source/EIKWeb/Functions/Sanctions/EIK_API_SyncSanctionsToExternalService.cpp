ad8330f9770e4aaf9c101093e55d5437cfd90628
Betide Studio <codingwithdevesh@gmail.com>
2024-10-24 14:54:04 +0530

    [API/Sanctions] Sync Sanctions to External Service

---
+++ Source/EIKWeb/Functions/Sanctions/EIK_API_SyncSanctionsToExternalService.cpp
@@ -0,0 +1,36 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+
+#include "EIK_API_SyncSanctionsToExternalService.h"
+
+UEIK_API_SyncSanctionsToExternalService* UEIK_API_SyncSanctionsToExternalService::SyncSanctionsToExternalService(
+	FString Authorization, FString LastLogId)
+{
+	UEIK_API_SyncSanctionsToExternalService* Proxy = NewObject<UEIK_API_SyncSanctionsToExternalService>();
+	Proxy->Var_Authorization = Authorization;
+	Proxy->Var_LastLogId = LastLogId;
+	return Proxy;
+}
+
+void UEIK_API_SyncSanctionsToExternalService::Activate()
+{
+	Super::Activate();
+	FString URL = FString::Printf(TEXT("%s/sanctions/v1/sync"), *APIEndpoint);
+	if (!Var_LastLogId.IsEmpty())
+	{
+		URL.Append(FString::Printf(TEXT("?lastLogId=%s"), *Var_LastLogId));
+	}
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = FHttpModule::Get().CreateRequest();
+	HttpRequest->SetVerb(TEXT("GET"));
+	HttpRequest->SetURL(URL);
+	if (Var_Authorization.Contains("Bearer"))
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), Var_Authorization);
+	}
+	else
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), FString::Printf(TEXT("Bearer %s"), *Var_Authorization));
+	}
+	HttpRequest->OnProcessRequestComplete().BindUObject(this, &UEIK_API_SyncSanctionsToExternalService::OnResponseReceived);
+	HttpRequest->ProcessRequest();
+}
---
+++ Source/EIKWeb/Functions/Sanctions/EIK_API_SyncSanctionsToExternalService.h
@@ -0,0 +1,26 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EIK_BaseWebApi.h"
+#include "EIK_API_SyncSanctionsToExternalService.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EIKWEB_API UEIK_API_SyncSanctionsToExternalService : public UEIK_BaseWebApi
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit|Web")
+	static UEIK_API_SyncSanctionsToExternalService* SyncSanctionsToExternalService(FString Authorization, FString LastLogId);
+
+private:
+	virtual void Activate() override;
+	FString Var_Authorization;
+	FString Var_LastLogId;
+};
