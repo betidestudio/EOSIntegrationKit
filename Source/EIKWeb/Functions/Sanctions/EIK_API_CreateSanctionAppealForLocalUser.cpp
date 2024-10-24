eea58a5c5541a123a13be82f432bd97ef655771d
Betide Studio <codingwithdevesh@gmail.com>
2024-10-24 14:51:31 +0530

    [API/Sanctions] Create Sanction Appeal for Local User

---
+++ Source/EIKWeb/Functions/Sanctions/EIK_API_CreateSanctionAppealForLocalUser.cpp
@@ -0,0 +1,49 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+
+#include "EIK_API_CreateSanctionAppealForLocalUser.h"
+
+UEIK_API_CreateSanctionAppealForLocalUser* UEIK_API_CreateSanctionAppealForLocalUser::CreateSanctionAppealForLocalUser(
+	FString Authorization, FString DeploymentId, FString ProductUserId, FString ReferenceId, FString Reason)
+{
+	UEIK_API_CreateSanctionAppealForLocalUser* Proxy = NewObject<UEIK_API_CreateSanctionAppealForLocalUser>();
+	Proxy->Var_Authorization = Authorization;
+	Proxy->Var_DeploymentId = DeploymentId;
+	Proxy->Var_ProductUserId = ProductUserId;
+	Proxy->Var_ReferenceId = ReferenceId;
+	Proxy->Var_Reason = Reason;
+	return Proxy;
+}
+
+void UEIK_API_CreateSanctionAppealForLocalUser::Activate()
+{
+	Super::Activate();
+	FString URL = FString::Printf(TEXT("%s/sanctions/v1/%s/productUser/%s/appeals"), *APIEndpoint, *Var_DeploymentId, *Var_ProductUserId);
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = FHttpModule::Get().CreateRequest();
+	HttpRequest->SetVerb(TEXT("POST"));
+	HttpRequest->SetURL(URL);
+	HttpRequest->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
+	if (Var_Authorization.Contains("Bearer"))
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), Var_Authorization);
+	}
+	else
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), FString::Printf(TEXT("Bearer %s"), *Var_Authorization));
+	}
+	TSharedPtr<FJsonObject> RequestBodyJson = MakeShareable(new FJsonObject);
+	if (!Var_ReferenceId.IsEmpty())
+	{
+		RequestBodyJson->SetStringField(TEXT("referenceId"), Var_ReferenceId);
+	}
+	if (!Var_Reason.IsEmpty())
+	{
+		RequestBodyJson->SetStringField(TEXT("reason"), Var_Reason);
+	}
+	FString RequestBodyString;
+	TSharedRef<TJsonWriter<TCHAR>> JsonWriter = TJsonWriterFactory<>::Create(&RequestBodyString);
+	FJsonSerializer::Serialize(RequestBodyJson.ToSharedRef(), JsonWriter);
+	HttpRequest->SetContentAsString(RequestBodyString);
+	HttpRequest->OnProcessRequestComplete().BindUObject(this, &UEIK_API_CreateSanctionAppealForLocalUser::OnResponseReceived);
+	HttpRequest->ProcessRequest();
+}
---
+++ Source/EIKWeb/Functions/Sanctions/EIK_API_CreateSanctionAppealForLocalUser.h
@@ -0,0 +1,29 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EIK_BaseWebApi.h"
+#include "EIK_API_CreateSanctionAppealForLocalUser.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EIKWEB_API UEIK_API_CreateSanctionAppealForLocalUser : public UEIK_BaseWebApi
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit|Web")
+	static UEIK_API_CreateSanctionAppealForLocalUser* CreateSanctionAppealForLocalUser(FString Authorization, FString DeploymentId, FString ProductUserId, FString ReferenceId, FString Reason);
+
+private:
+	virtual void Activate() override;
+	FString Var_Authorization;
+	FString Var_DeploymentId;
+	FString Var_ProductUserId;
+	FString Var_ReferenceId;
+	FString Var_Reason;
+};
