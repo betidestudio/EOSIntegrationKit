7fc4c70aea34b92f9e7a2277b4ca21745e4cdf3c
Betide Studio <codingwithdevesh@gmail.com>
2024-10-25 20:44:23 +0530

    [API/Ecom] Token Based Entitlement Verification

---
+++ Source/EIKWeb/Functions/Ecom/EIK_API_TokenBasedEntitlementVerification.cpp
@@ -0,0 +1,4 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+
+#include "EIK_API_TokenBasedEntitlementVerification.h"
---
+++ Source/EIKWeb/Functions/Ecom/EIK_API_TokenBasedEntitlementVerification.h
@@ -0,0 +1,16 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EIK_BaseWebApi.h"
+#include "EIK_API_TokenBasedEntitlementVerification.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EIKWEB_API UEIK_API_TokenBasedEntitlementVerification : public UEIK_BaseWebApi
+{
+	GENERATED_BODY()
+};
