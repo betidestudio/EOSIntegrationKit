2509dce1029bcf5acf357ec74bf58eee8302fbe3
Betide Studio <codingwithdevesh@gmail.com>
2024-10-26 01:59:04 +0530

    [API/Ecom] Query Clawbacks

---
+++ Source/EIKWeb/Functions/Ecom/EIK_API_QueryClawbacks.cpp
@@ -0,0 +1,35 @@
+﻿// Copyright (c) 2024 Betide Studio. All Rights Reserved.
+
+
+#include "EIK_API_QueryClawbacks.h"
+
+UEIK_API_QueryClawbacks* UEIK_API_QueryClawbacks::QueryClawbacks(FString Authorization, FString NameSpace,
+	FString ClawbackDate, int32 Count)
+{
+	UEIK_API_QueryClawbacks* Node = NewObject<UEIK_API_QueryClawbacks>();
+	Node->Var_Authorization = Authorization;
+	Node->Var_NameSpace = NameSpace;
+	Node->Var_ClawbackDate = ClawbackDate;
+	Node->Var_Count = Count;
+	return Node;
+}
+
+void UEIK_API_QueryClawbacks::Activate()
+{
+	Super::Activate();
+	FString URL = FString::Printf(TEXT("%s/epic/ecom/v3/namespace/%s/entitlements/clawbacks?clawbackDate=%s&count=%d"), *APIEndpoint, *Var_NameSpace, *Var_ClawbackDate, Var_Count);
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = FHttpModule::Get().CreateRequest();
+	HttpRequest->SetVerb(TEXT("GET"));
+	HttpRequest->SetURL(URL);
+	HttpRequest->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
+	if (Var_Authorization.Contains("Bearer"))
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), Var_Authorization);
+	}
+	else
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), FString::Printf(TEXT("Bearer %s"), *Var_Authorization));
+	}
+	HttpRequest->OnProcessRequestComplete().BindUObject(this, &UEIK_API_QueryClawbacks::OnResponseReceived);
+	HttpRequest->ProcessRequest();
+}
---
+++ Source/EIKWeb/Functions/Ecom/EIK_API_QueryClawbacks.h
@@ -0,0 +1,29 @@
+﻿// Copyright (c) 2024 Betide Studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EIK_BaseWebApi.h"
+#include "EIK_API_QueryClawbacks.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EIKWEB_API UEIK_API_QueryClawbacks : public UEIK_BaseWebApi
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit|Web API|Ecom")
+	static UEIK_API_QueryClawbacks* QueryClawbacks(FString Authorization, FString NameSpace, FString ClawbackDate, int32 Count=10);
+
+private:
+	
+	virtual void Activate() override;
+	FString Var_Authorization;
+	FString Var_NameSpace;
+	FString Var_ClawbackDate;
+	int32 Var_Count;
+};
