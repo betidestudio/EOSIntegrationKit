35fa12bd5f3a76ca2b1f306c3b564c9bed3bb86b
Betide Studio <codingwithdevesh@gmail.com>
2024-10-26 01:58:21 +0530

    [API/Ecom] Verify Entitlement Verification Token

---
+++ Source/EIKWeb/Functions/Ecom/EIK_API_VerifyEntitlementVerificationToken.cpp
@@ -0,0 +1,24 @@
+﻿// Copyright (c) 2024 Betide Studio. All Rights Reserved.
+
+
+#include "EIK_API_VerifyEntitlementVerificationToken.h"
+
+UEIK_API_VerifyEntitlementVerificationToken* UEIK_API_VerifyEntitlementVerificationToken::
+VerifyEntitlementVerificationToken(FString KeyId)
+{
+	UEIK_API_VerifyEntitlementVerificationToken* Node = NewObject<UEIK_API_VerifyEntitlementVerificationToken>();
+	Node->Var_KeyId = KeyId;
+	return Node;
+}
+
+void UEIK_API_VerifyEntitlementVerificationToken::Activate()
+{
+	Super::Activate();
+	FString URL = FString::Printf(TEXT("https://ecommerceintegration-public-service-ecomprod02.ol.epicgames.com/ecommerceintegration/api/public/publickeys/%s"), *Var_KeyId);
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = FHttpModule::Get().CreateRequest();
+	HttpRequest->SetVerb(TEXT("GET"));
+	HttpRequest->SetURL(URL);
+	HttpRequest->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
+	HttpRequest->OnProcessRequestComplete().BindUObject(this, &UEIK_API_VerifyEntitlementVerificationToken::OnResponseReceived);
+	HttpRequest->ProcessRequest();
+}
---
+++ Source/EIKWeb/Functions/Ecom/EIK_API_VerifyEntitlementVerificationToken.h
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2024 Betide Studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EIK_BaseWebApi.h"
+#include "EIK_API_VerifyEntitlementVerificationToken.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EIKWEB_API UEIK_API_VerifyEntitlementVerificationToken : public UEIK_BaseWebApi
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit|Web API|Ecom")
+	static UEIK_API_VerifyEntitlementVerificationToken* VerifyEntitlementVerificationToken(FString KeyId);
+
+private:
+	
+	virtual void Activate() override;
+	FString Var_KeyId;
+};
