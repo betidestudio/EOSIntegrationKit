8ddbb159c6a4b21d20c45d9c5ba1d4a1cb6b3b11
Betide Studio <codingwithdevesh@gmail.com>
2024-10-26 04:21:04 +0530

    Add Ping Host Object

---
+++ Source/OnlineSubsystemEIK/AsyncFunctions/Beacons/PingHostObject.cpp
@@ -0,0 +1,34 @@
+#include "PingHostObject.h"
+#include "PingClient.h"
+
+DEFINE_LOG_CATEGORY(LogPingHostObject)
+
+APingHostObject::APingHostObject(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	ClientBeaconActorClass = APingClient::StaticClass();
+	BeaconTypeName = ClientBeaconActorClass->GetName();
+
+	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bAllowTickOnDedicatedServer = true;
+	PrimaryActorTick.bStartWithTickEnabled = true;
+}
+
+AOnlineBeaconClient* APingHostObject::SpawnBeaconActor(UNetConnection* ClientConnection)
+{
+	return Super::SpawnBeaconActor(ClientConnection);
+}
+
+void APingHostObject::OnClientConnected(AOnlineBeaconClient* ClientActor, UNetConnection* ClientConnection)
+{
+	Super::OnClientConnected(ClientActor, ClientConnection);
+	APingClient* BeaconClient = Cast<APingClient>(ClientActor);
+	if (BeaconClient != nullptr)
+	{
+		BeaconClient->ClientPingBegin();
+	}
+}
+
+bool APingHostObject::Init()
+{
+	return true;
+}
---
+++ Source/OnlineSubsystemEIK/AsyncFunctions/Beacons/PingHostObject.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "OnlineBeaconHostObject.h"
+#include "PingHostObject.generated.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogPingHostObject, Log, All);
+
+/**
+* 
+*/
+UCLASS(Blueprintable, BlueprintType, Transient, NotPlaceable, Config = Engine)
+class ONLINESUBSYSTEMEIK_API APingHostObject : public AOnlineBeaconHostObject
+{
+	GENERATED_BODY()
+
+public:
+	APingHostObject(const FObjectInitializer& ObjectInitializer);
+
+	virtual AOnlineBeaconClient* SpawnBeaconActor(class UNetConnection* ClientConnection) override;
+
+	virtual void OnClientConnected(class AOnlineBeaconClient* ClientActor, class UNetConnection* ClientConnection) override;
+
+	virtual bool Init();
+};
