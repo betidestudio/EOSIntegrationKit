db3a9ff09b2ee59430d7f28ac09131035f14697b
Betide Studio <codingwithdevesh@gmail.com>
2024-10-26 04:21:26 +0530

    Add Ping Host

---
+++ Source/OnlineSubsystemEIK/AsyncFunctions/Beacons/PingHost.cpp
@@ -0,0 +1,22 @@
+#include "PingHost.h"
+
+DEFINE_LOG_CATEGORY(LogPingHost)
+
+APingHost::APingHost(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	BeaconState = EBeaconState::AllowRequests;
+}
+
+bool APingHost::InitializeHost()
+{
+	bInitSuccess = InitHost();
+	return bInitSuccess;
+}
+
+void APingHost::RegisterHostObject(AOnlineBeaconHostObject* HostObject)
+{
+	if (bInitSuccess)
+	{
+		RegisterHost(HostObject);
+	}
+}
---
+++ Source/OnlineSubsystemEIK/AsyncFunctions/Beacons/PingHost.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "OnlineBeaconHost.h"
+#include "PingHost.generated.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogPingHost, Log, All);
+
+/**
+* 
+*/
+UCLASS(Blueprintable, BlueprintType, Transient, NotPlaceable, Config=Engine)
+class ONLINESUBSYSTEMEIK_API APingHost : public AOnlineBeaconHost
+{
+	GENERATED_BODY()	
+
+public:
+	APingHost(const FObjectInitializer& ObjectInitializer);
+
+	UFUNCTION(BlueprintCallable, Category=Ping)
+	bool InitializeHost();
+
+	UFUNCTION(BlueprintCallable, Category=Ping)
+	void RegisterHostObject(AOnlineBeaconHostObject* HostObject);
+
+protected:
+	bool bInitSuccess;
+};
