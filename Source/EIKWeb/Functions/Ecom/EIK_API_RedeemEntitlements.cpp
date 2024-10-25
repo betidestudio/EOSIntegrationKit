0bcb4d94863cafc5b2a3fa2453a0ef9dc0463687
Betide Studio <codingwithdevesh@gmail.com>
2024-10-26 01:58:37 +0530

    [API/Ecom] Redeem Entitlements

---
+++ Source/EIKWeb/Functions/Ecom/EIK_API_RedeemEntitlements.cpp
@@ -0,0 +1,55 @@
+﻿// Copyright (c) 2024 Betide Studio. All Rights Reserved.
+
+
+#include "EIK_API_RedeemEntitlements.h"
+
+UEIK_API_RedeemEntitlements* UEIK_API_RedeemEntitlements::RedeemEntitlements(FString Authorization, FString IdentityId,
+	TArray<FString> EntitlementIds, FString SandboxId)
+{
+	UEIK_API_RedeemEntitlements* Node = NewObject<UEIK_API_RedeemEntitlements>();
+	Node->Var_Authorization = Authorization;
+	Node->Var_IdentityId = IdentityId;
+	Node->Var_EntitlementIds = EntitlementIds;
+	Node->Var_SandboxId = SandboxId;
+	return Node;
+}
+
+void UEIK_API_RedeemEntitlements::Activate()
+{
+	Super::Activate();
+	FString URL = FString::Printf(TEXT("%s/epic/ecom/v3/identities/%s/entitlements/redeem"), *APIEndpoint, *Var_IdentityId);
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = FHttpModule::Get().CreateRequest();
+	HttpRequest->SetVerb(TEXT("PUT"));
+	HttpRequest->SetURL(URL);
+	HttpRequest->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
+	if (Var_Authorization.Contains("Bearer"))
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), Var_Authorization);
+	}
+	else
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), FString::Printf(TEXT("Bearer %s"), *Var_Authorization));
+	}
+	//Make Json Body
+	TSharedPtr<FJsonObject> RequestBodyJson = MakeShareable(new FJsonObject);
+	if(!Var_SandboxId.IsEmpty())
+	{
+		RequestBodyJson->SetStringField(TEXT("sandboxId"), Var_SandboxId);
+	}
+	TArray<TSharedPtr<FJsonValue>> EntitlementIdsJson;
+	if (Var_EntitlementIds.Num() > 0)
+	{
+		for (auto EntitlementId : Var_EntitlementIds)
+		{
+			TSharedPtr<FJsonValue> EntitlementIdJson = MakeShareable(new FJsonValueString(EntitlementId));
+			EntitlementIdsJson.Add(EntitlementIdJson);
+		}
+	}
+	RequestBodyJson->SetArrayField(TEXT("entitlementIds"), EntitlementIdsJson);
+	FString RequestBodyString;
+	TSharedRef<TJsonWriter<TCHAR>> JsonWriter = TJsonWriterFactory<>::Create(&RequestBodyString);
+	FJsonSerializer::Serialize(RequestBodyJson.ToSharedRef(), JsonWriter);
+	HttpRequest->SetContentAsString(RequestBodyString);
+	HttpRequest->OnProcessRequestComplete().BindUObject(this, &UEIK_API_RedeemEntitlements::OnResponseReceived);
+	HttpRequest->ProcessRequest();
+}
---
+++ Source/EIKWeb/Functions/Ecom/EIK_API_RedeemEntitlements.h
@@ -0,0 +1,29 @@
+﻿// Copyright (c) 2024 Betide Studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EIK_BaseWebApi.h"
+#include "EIK_API_RedeemEntitlements.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EIKWEB_API UEIK_API_RedeemEntitlements : public UEIK_BaseWebApi
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit|Web API|Ecom")
+	static UEIK_API_RedeemEntitlements* RedeemEntitlements(FString Authorization, FString IdentityId, TArray<FString> EntitlementIds, FString SandboxId);
+
+private:
+	
+	virtual void Activate() override;
+	FString Var_IdentityId;
+	FString Var_SandboxId;
+	FString Var_Authorization;
+	TArray<FString> Var_EntitlementIds;
+};
