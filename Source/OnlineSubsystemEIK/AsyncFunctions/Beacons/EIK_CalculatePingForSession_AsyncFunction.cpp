5b6320a090f00b33add381ae8e777c75a860bac8
Betide Studio <codingwithdevesh@gmail.com>
2024-10-26 04:21:47 +0530

    Calculate Ping for Session Added

---
+++ Source/OnlineSubsystemEIK/AsyncFunctions/Beacons/EIK_CalculatePingForSession_AsyncFunction.cpp
@@ -0,0 +1,65 @@
+﻿// Copyright (c) 2024 Betide Studio. All Rights Reserved.
+
+
+#include "EIK_CalculatePingForSession_AsyncFunction.h"
+
+#include "PingClient.h"
+
+UEIK_CalculatePingForSession_AsyncFunction* UEIK_CalculatePingForSession_AsyncFunction::CalculatePingForSession(
+	FSessionFindStruct SessionFindStruct)
+{
+	UEIK_CalculatePingForSession_AsyncFunction* BlueprintNode = NewObject<UEIK_CalculatePingForSession_AsyncFunction>();
+	BlueprintNode->Var_SessionFindStruct = SessionFindStruct;
+	return BlueprintNode;
+}
+
+void UEIK_CalculatePingForSession_AsyncFunction::OnPingComplete(int32 Ping, bool bSuccess)
+{
+	if(bSuccess)
+	{
+		OnSuccess.Broadcast(Ping);
+	}
+	else
+	{
+		OnFailure.Broadcast(Ping);
+	}
+	SetReadyToDestroy();
+#if ENGINE_MAJOR_VERSION == 5
+	MarkAsGarbage();
+#else
+	MarkPendingKill();
+#endif
+}
+
+void UEIK_CalculatePingForSession_AsyncFunction::Activate()
+{
+	Super::Activate();
+	if(GetWorld())
+	{
+		if(APingClient* PingClient = GetWorld()->SpawnActor<APingClient>(APingClient::StaticClass()))
+		{
+			FEIK_PingComplete OnPingComplete;
+			OnPingComplete.BindDynamic(this, &UEIK_CalculatePingForSession_AsyncFunction::OnPingComplete);
+			if(!PingClient->ConnectToSession(Var_SessionFindStruct.SessionResult, OnPingComplete))
+			{
+				OnFailure.Broadcast(0);
+				SetReadyToDestroy();
+#if ENGINE_MAJOR_VERSION == 5
+				MarkAsGarbage();
+#else
+				MarkPendingKill();
+#endif
+			}
+		}
+		else
+		{
+			OnFailure.Broadcast(0);
+			SetReadyToDestroy();
+		}
+	}
+	else
+	{
+		OnFailure.Broadcast(0);
+		SetReadyToDestroy();
+	}
+}
---
+++ Source/OnlineSubsystemEIK/AsyncFunctions/Beacons/EIK_CalculatePingForSession_AsyncFunction.h
@@ -0,0 +1,34 @@
+﻿// Copyright (c) 2024 Betide Studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "OnlineSubsystemEIK/Subsystem/EIK_Subsystem.h"
+#include "EIK_CalculatePingForSession_AsyncFunction.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FEIK_OnCalculatePingForSessionComplete, int32, Ping);
+UCLASS()
+class ONLINESUBSYSTEMEIK_API UEIK_CalculatePingForSession_AsyncFunction : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit | Beacons | Ping", meta = (BlueprintInternalUseOnly = "true"), DisplayName = "Calculate Ping For Session")
+	static UEIK_CalculatePingForSession_AsyncFunction* CalculatePingForSession(FSessionFindStruct SessionFindStruct);
+
+	UPROPERTY(BlueprintAssignable, Category = "EOS Integration Kit | Beacons | Ping")
+	FEIK_OnCalculatePingForSessionComplete OnSuccess;
+
+	UPROPERTY(BlueprintAssignable, Category = "EOS Integration Kit | Beacons | Ping")
+	FEIK_OnCalculatePingForSessionComplete OnFailure;
+
+private:
+	UFUNCTION()
+	void OnPingComplete(int32 Ping, bool bSuccess);
+	virtual void Activate() override;
+
+	FSessionFindStruct Var_SessionFindStruct;
+	
+};
