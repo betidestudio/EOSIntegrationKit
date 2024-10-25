fd47ab360abc5c710a2fdb8815c0b490c2775171
Betide Studio <codingwithdevesh@gmail.com>
2024-10-25 20:44:06 +0530

    [API/Ecom] Direct Ownership Verification

---
+++ Source/EIKWeb/Functions/Ecom/EIK_API_DirectOwnershipVerification.cpp
@@ -0,0 +1,52 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+
+#include "EIK_API_DirectOwnershipVerification.h"
+
+UEIK_API_DirectOwnershipVerification* UEIK_API_DirectOwnershipVerification::DirectOwnershipVerification(
+	FString Authorization, FString CurrentAccountId, TArray<FString> NsCatalogItemIds, FString SandboxId)
+{
+	UEIK_API_DirectOwnershipVerification* Node = NewObject<UEIK_API_DirectOwnershipVerification>();
+	Node->Var_Authorization = Authorization;
+	Node->Var_CurrentAccountId = CurrentAccountId;
+	Node->Var_NsCatalogItemIds = NsCatalogItemIds;
+	Node->Var_SandboxId = SandboxId;
+	return Node;
+}
+
+void UEIK_API_DirectOwnershipVerification::Activate()
+{
+	Super::Activate();
+	FString URL = FString::Printf(TEXT("%s/epic/ecom/v3/platforms/EPIC/identities/%s/ownership"), *APIEndpoint, *Var_CurrentAccountId);
+	//https://api.epicgames.dev/epic/ecom/v3/platforms/EPIC/identities/{{currentAccountId}}/ownership?nsCatalogItemId={{sandboxId:catalogItemId}}
+	if (Var_NsCatalogItemIds.Num() > 0)
+	{
+		URL.Append("?nsCatalogItemId=");
+		for (int i = 0; i < Var_NsCatalogItemIds.Num(); i++)
+		{
+			URL.Append(FString::Printf(TEXT("%s%s"), *Var_SandboxId, *Var_NsCatalogItemIds[i]));
+			if (i < Var_NsCatalogItemIds.Num() - 1)
+			{
+				URL.Append(",");
+			}
+		}
+	}
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = FHttpModule::Get().CreateRequest();
+	HttpRequest->SetVerb(TEXT("GET"));
+	HttpRequest->SetURL(URL);
+	HttpRequest->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
+	if (Var_Authorization.Contains("Bearer"))
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), Var_Authorization);
+	}
+	else
+	{
+		HttpRequest->SetHeader(TEXT("Authorization"), FString::Printf(TEXT("Bearer %s"), *Var_Authorization));
+	}
+	HttpRequest->OnProcessRequestComplete().BindUObject(this, &UEIK_API_DirectOwnershipVerification::OnResponseReceived);
+	HttpRequest->ProcessRequest();
+	
+	
+	
+
+}
---
+++ Source/EIKWeb/Functions/Ecom/EIK_API_DirectOwnershipVerification.h
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EIK_BaseWebApi.h"
+#include "EIK_API_DirectOwnershipVerification.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EIKWEB_API UEIK_API_DirectOwnershipVerification : public UEIK_BaseWebApi
+{
+	GENERATED_BODY()
+
+public:
+	//The endpoint to check if a user owns an item, or a list of items.
+	UFUNCTION(BlueprintCallable, Category = "EOS Integration Kit|Web API|AntiCheat")
+	static UEIK_API_DirectOwnershipVerification* DirectOwnershipVerification(FString Authorization, FString CurrentAccountId, TArray<FString> NsCatalogItemIds, FString SandboxId);
+
+private:
+	virtual void Activate() override;
+	FString Var_Authorization;
+	FString Var_CurrentAccountId;
+	TArray<FString> Var_NsCatalogItemIds;
+	FString Var_SandboxId;
+};
