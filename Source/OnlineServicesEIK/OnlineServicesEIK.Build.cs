2765131a2f4d4c0698bafb4a684e79686071adf2
Betide Studio <codingwithdevesh@gmail.com>
2024-11-02 17:40:30 +0530

    Empty Online Services EIK Module added

---
+++ Source/OnlineServicesEIK/OnlineServicesEIK.Build.cs
@@ -0,0 +1,26 @@
+﻿using UnrealBuildTool;
+
+public class OnlineServicesEIK : ModuleRules
+{
+    public OnlineServicesEIK(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
---
+++ Source/OnlineServicesEIK/Private/OnlineServicesEIK.cpp
@@ -0,0 +1,17 @@
+﻿#include "OnlineServicesEIK.h"
+
+#define LOCTEXT_NAMESPACE "FOnlineServicesEIKModule"
+
+void FOnlineServicesEIKModule::StartupModule()
+{
+    
+}
+
+void FOnlineServicesEIKModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FOnlineServicesEIKModule, OnlineServicesEIK)
